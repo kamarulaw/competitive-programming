0f1f9c7ec39e3e7a3e6ffb40a3207c531d52a8ae
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2022-02-23 21:53:25 -0500

    Add files via upload

---
+++ exercises/cpp/findsubsequenceoflengthkwiththelargestsum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+      vector<int> res; 
+      map<int, int> m;
+      priority_queue<int> pq;  
+      
+      for (int x: nums) 
+        pq.push(x);
+      int kc = k;
+      
+      while (kc != 0) { 
+        m[pq.top()]++;
+        pq.pop(); 
+        kc--; 
+      }
+      
+      for (int i = 0; i < nums.size(); i++) { 
+        if (res.size() == k)
+          break; 
+        if (m[nums[i]] > 0) { 
+          res.push_back(nums[i]);
+          m[nums[i]]--; 
+        }
+      }
+      return res; 
+    }     
+};
