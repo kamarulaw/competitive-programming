2d18bf3168dcbc331b5bd3b02056809615f0688b
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2022-02-28 13:58:40 -0500

    Update graphs.cpp

--- tutorials/cpp/graphs.cpp
+++ tutorials/cpp/graphs.cpp
@@ -1,22 +1,44 @@
 #include <queue>
+#include <stack>
 #include <vector>
 #include <iostream>
 using namespace std;
 
-// TEST GRAPHS
-vector<vector<int>> g1; 
-vector<vector<int>> g2; 
+// Depth First Search
+void dfs(vector<vector<int>> graph, int start) { 
+  stack<int> s; 
+  vector<bool> vis; 
+  int n = graph.size(); 
+  for (int i = 0; i <= n; i++)
+    vis.push_back(false);
+  s.push(start);
+  vis[0] = true; vis[start] = true; 
+  cout << start << " "; 
+  while (!s.empty()) { 
+    int top = s.top(); 
+    s.pop(); 
+    for (int i = 0; i < graph[top].size(); i++) { 
+      int curr = graph[top][i];
+      if (vis[curr] == false) { 
+        s.push(curr);
+        vis[curr] = true; 
+        cout << curr << " ";
+      }
+    }
+  }
+  cout << endl; 
+  return;   
+}
 
 // Breadth First Search
 void bfs(vector<vector<int>> graph, int start) { 
-  cout << "GOT HERE" <<endl;
   queue<int> q; 
   vector<bool> vis; 
   int n = graph.size(); 
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i <= n; i++)
     vis.push_back(false);
   q.push(start);
-  vis[start] = true;
+  vis[0] = true; vis[start] = true; 
   cout << start << " "; 
   while (!q.empty()) { 
     int front = q.front(); 
@@ -30,36 +52,47 @@ void bfs(vector<vector<int>> graph, int start) {
       }
     }
   }
+  cout << endl; 
   return;   
 }
   
-
 // UTIL 
-void addEdge(vector<vector<int>>g,int s,int d) { 
-  cout << "gh" << endl; 
+vector<vector<int>> createGraph(int n) { 
+  vector<int> evec; 
+  vector<vector<int>> graph; 
+  for (int i = 0; i <= n; i++) 
+    graph.push_back(evec);
+  return graph; 
+}
+
+vector<vector<int>> addEdge(vector<vector<int>>g,int s,int d) { 
   g[s].push_back(d);
   g[d].push_back(s);
-  cout << "gh2" <<endl; 
-  return; 
+  return g; 
 }
 
-void initGraph(vector<vector<int>> g, int n) { 
-  cout << "GOT HERE TOO" << endl; 
-  vector<int> evec; 
-  for (int i = 0; i <= n; i++) 
-    g.push_back(evec);
-  return; 
-}
-
-int main() {
-  // Create graph1
-  initGraph(g1,7);
-  addEdge(g1,1,2);
-  addEdge(g1,1,3);
-  addEdge(g1,2,4);
-  addEdge(g1,2,5);
-  addEdge(g1,3,6);
-  addEdge(g1,3,7);
+int main() 
+{
+  // Create Graph
+  vector<vector<int>> g1 = createGraph(15);
+  g1 = addEdge(g1,1,2);
+  g1 = addEdge(g1,1,3);
+  g1 = addEdge(g1,2,4);
+  g1 = addEdge(g1,2,5);
+  g1 = addEdge(g1,3,6);
+  g1 = addEdge(g1,3,7);
+  g1 = addEdge(g1,4,8);
+  g1 = addEdge(g1,4,9);
+  g1 = addEdge(g1,5,10);
+  g1 = addEdge(g1,5,11);
+  g1 = addEdge(g1,6,12);
+  g1 = addEdge(g1,6,13);
+  g1 = addEdge(g1,7,14);
+  g1 = addEdge(g1,7,15);
   
+  // Test
   bfs(g1,1);
+  dfs(g1,1);
+  
+  return 0;
 }
