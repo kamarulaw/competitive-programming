99692093ac163cfa1fe6d7888bc21da1442726a7
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-07 11:41:48 -0400

    Update partial_asteroidcollision.cpp

--- exercises/cpp/partial_asteroidcollision.cpp
+++ exercises/cpp/partial_asteroidcollision.cpp
@@ -1,10 +1,12 @@
+/* potential Fix: change some of the for loops to while loops*/
+
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> result;
         
         result.push_back(asteroids[0]); 
-        
+
         for (int i = 1; i < asteroids.size(); i++)
         {
             if (result[i-1] < 0 && asteroids[i] > 0) {
@@ -18,6 +20,7 @@ public:
                 {
                     result.erase(result.begin() + (i-1));
                 }
+
             } else if (result[i-1] > 0 && asteroids[i] < 0) {
                 if (result[i-1] < asteroids[i] * -1)
                 {
@@ -29,6 +32,7 @@ public:
                 {
                     result.erase(result.begin() + (i-1));
                 }
+
             } else {
                 result.push_back(asteroids[i]);
             }
