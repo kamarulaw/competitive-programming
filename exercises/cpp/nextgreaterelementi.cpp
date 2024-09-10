e85b0d5db7d819b17975f5ce6d3d1217a4e9ef33
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-10 14:15:59 -0400

    Update nextgreaterelementi.cpp

--- exercises/cpp/nextgreaterelementi.cpp
+++ exercises/cpp/nextgreaterelementi.cpp
@@ -1,3 +1,5 @@
+/* SOLUTION 1*/ 
+/* All tests passed */
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
@@ -22,5 +24,43 @@ public:
             result.push_back(m[nums1[i]]);
         }
         return result; 
+    }
+};
+
+/* SOLUTION 2*/ 
+/* All tests passed */
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
+        map<int, int> m;
+        vector<int> result;
+
+        for (int i = 0; i < nums2.size(); i++)
+        {
+            m[nums2[i]] = -1;
+        }
+
+        for (int i = nums2.size()-1; i >= 0; i--)
+        {
+            while (s.size() > 0 && nums2[i] >= nums2[s.top()]) 
+            {
+                s.pop();
+            }
+
+            if (s.size() > 0)
+            {
+                m[nums2[i]] = nums2[s.top()];
+            }          
+
+            s.push(i);
+        }
+
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            result.push_back(m[nums1[i]]);
+        }
+        
+        return result;
     }
 };
