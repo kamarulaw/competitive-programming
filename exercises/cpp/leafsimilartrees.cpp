96de1cd400678220e9ce32d657f7279af734471e
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-11 14:04:57 -0400

    Update leafsimilartrees.cpp

--- exercises/cpp/leafsimilartrees.cpp
+++ exercises/cpp/leafsimilartrees.cpp
@@ -1,14 +1,5 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+/* SOLUTION 1 */ 
+/* All tests passed */
 class Solution {
 public:
     vector<int> r1; 
@@ -38,3 +29,64 @@ public:
       return true; 
     }
 };
+
+/* SOLUTION 2 */
+/* All tests passed */
+class Solution {
+public:
+    vector<int> lvs1; 
+    vector<int> lvs2;
+
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) 
+    {
+        findlvs(root1,1); 
+        findlvs(root2,2);
+
+        if (lvs1.size() != lvs2.size())
+        {
+            return false;
+        }
+
+        for (int i = 0; i < lvs1.size(); i++)
+        {
+            if (lvs1[i] != lvs2[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void findlvs(TreeNode* root, int seqnum)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        if (root->left == NULL && root->right == NULL)
+        {
+            if (seqnum == 1)
+            {
+                lvs1.push_back(root->val);
+            }
+            else
+            {
+                lvs2.push_back(root->val);
+            }
+        }
+        findlvs(root->left,seqnum); 
+        findlvs(root->right,seqnum);
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
