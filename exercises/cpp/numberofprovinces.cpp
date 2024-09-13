914b85ca69a2061366f7836724a9c437685f5c79
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-13 13:07:24 -0400

    Update numberofprovinces.cpp

--- exercises/cpp/numberofprovinces.cpp
+++ exercises/cpp/numberofprovinces.cpp
@@ -1,4 +1,4 @@
-/* SOLUTION 1 8> 
+/* SOLUTION 1 */ 
 /* WRONG ANSWER ON CASE 5 of 114 */
 class Solution {
 public:
@@ -59,5 +59,76 @@ public:
             provmax = max(provmax, nodeprovincenums[i]);
         }
         return islands + provmax;
+    }
+};
+
+/* SOLUTION 2 */ 
+/* WRONG ANSWER ON CASE 20 of 114 */
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) 
+    {
+        int numprovinces = 0;
+        int n = isConnected.size();
+        
+        vector<int> v;
+        vector<vector<int>> graph;
+    
+        for (int i = 0; i < n; i++)
+        {
+            graph.push_back(v);
+        }
+
+        stack<int> st;
+        vector<int> visited;
+
+        for (int i = 0; i < n; i++)
+        {
+            visited.push_back(false);
+        }
+
+        st.push(0);
+        visited[0] = true;
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (isConnected[i][j] == 1)
+                {
+                    graph[i].push_back(j); 
+                    graph[j].push_back(i);                
+                }
+            }
+        }
+
+        vector<int> provincenums;
+        for (int i = 0; i < n; i++)
+        {
+            provincenums.push_back(i);
+        }
+
+        while (st.size() > 0)
+        {
+            int top = st.top(); 
+
+            st.pop();
+
+            for (int i = 0; i < graph[top].size(); i++)
+            {
+                if (visited[graph[top][i]] == false)
+                {
+                    visited[graph[top][i]] = true;
+                    provincenums[graph[top][i]] = min(top, graph[top][i]);
+                }
+            } 
+        }
+
+        map<int,int> nps;
+        for (int i = 0; i < provincenums.size(); i++)
+        {
+            nps[provincenums[i]]++;  
+        }
+        return nps.size();
     }
 };
