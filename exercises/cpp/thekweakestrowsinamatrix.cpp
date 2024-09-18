d728a806e375b533438ff9c9c7421ae8123f43bf
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-18 15:54:27 -0400

    Update thekweakestrowsinamatrix.cpp

--- exercises/cpp/thekweakestrowsinamatrix.cpp
+++ exercises/cpp/thekweakestrowsinamatrix.cpp
@@ -1,3 +1,5 @@
+/* SOLUTION 1 */ 
+/* RUNTIME ERROR */
 class Solution {
 public:
   static bool comp(vector<int> a, vector<int> b) { 
@@ -27,4 +29,58 @@ public:
     }
     return result; 
   }
+};
+
+/* SOLUTOIN 2 */ 
+/* All tests passed */
+class Solution {
+public:
+    static bool pqcmp(pair<vector<int>,int> p1, pair<vector<int>,int> p2)
+    {
+        int p1s = 0;
+        int p2s = 0;
+
+        for (int i = 0; i < p1.first.size(); i++)
+        {
+            if (p1.first[i] == 1)
+            {
+                p1s++;
+            }
+            if (p2.first[i] == 1)
+            {
+                p2s++; 
+            }
+        }
+
+        if (p1s != p2s)
+        {
+            return p1s > p2s;
+        }
+        else
+        {
+            return p1.second > p2.second;
+        }
+    }
+
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<int> result;
+
+        priority_queue<pair<vector<int>,int>, vector<pair<vector<int>,int>>, bool(*)(pair<vector<int>,int>,pair<vector<int>,int>)> pq(pqcmp);
+      
+        for (int i = 0; i < mat.size(); i++)
+        {
+            pair<vector<int>,int> p;
+            p.first = mat[i]; 
+            p.second = i;
+
+            pq.push(p);
+        }
+        while (k > 0)
+        {
+            result.push_back(pq.top().second); 
+            pq.pop();
+            k--;
+        }
+        return result;
+    }
 };
