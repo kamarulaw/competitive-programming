eb5e8ab0c445463dea622c7ad0e884aa5670a0e5
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-19 17:14:06 -0400

    Update largestnumberafterdigitswapsbyparity.cpp

--- exercises/cpp/largestnumberafterdigitswapsbyparity.cpp
+++ exercises/cpp/largestnumberafterdigitswapsbyparity.cpp
@@ -37,3 +37,90 @@ public:
         return permutations[permutations.size()-1];
     }
 };
+
+/* SOLUTION 2 */ 
+/* progress so far */
+class Solution {
+public:
+    vector<int> oddindices;
+    vector<int> evenindices;
+    set<vector<int>> oddindexperms;
+    set<vector<int>> evenindexperms;
+    void largestIntegerUtil(int i, vector<int> source_vector, bool oddflag)
+    {
+        if (i==source_vector.size()-1)
+        {
+            if (oddflag == false)
+            {
+                evenindexperms.emplace(source_vector);
+            }
+            else 
+            {
+                oddindexperms.emplace(source_vector);
+            }
+            return;
+        }
+        for (int j = i; j < source_vector.size(); j++)
+        {
+            largestIntegerUtil(i+1,source_vector,oddflag);
+            swap(source_vector[i],source_vector[j]);
+            largestIntegerUtil(i+1,source_vector,oddflag);
+        }
+    }
+    
+    int largestInteger(int num) 
+    {
+        string num_ = to_string(num);
+        map<int,int> index_digit_map;
+        vector<int> candidates;
+
+        for (int i = 0; i < num_.length(); i++)
+        {
+            int chartoint = num_[i] - '0';
+            index_digit_map[i] = chartoint;         
+            if (chartoint%2 == 0)
+            {
+                evenindices.push_back(i);
+            }  
+            else 
+            {
+                oddindices.push_back(i);
+            }
+        }
+
+        vector<int> arr;
+        largestIntegerUtil(0,evenindices,false);
+        largestIntegerUtil(0,oddindices,true);
+
+        vector<vector<int>> oddindexpermsv(oddindexperms.begin(),oddindexperms.end());
+        vector<vector<int>> evenindexpermsv(evenindexperms.begin(),evenindexperms.end());
+
+        for (int i = 0; i < oddindexpermsv.size(); i++)
+        {
+            string numasstring = "";
+            vector<int> oip = oddindexpermsv[i];
+            for (int j = 0; j < evenindexperms.size(); j++)
+            {
+                vector<int> eip = evenindexpermsv[j];
+                int k_ = 0;
+                for (int k = 0; k < num_.length(); k++)
+                {
+                    if (index_digit_map[k_]%2 == 0)   
+                    {
+                        numasstring += char(index_digit_map[eip[0]]);
+                        eip.erase(eip.begin());
+                    }
+                    else 
+                    {
+                        numasstring += char(index_digit_map[oip[0]]); 
+                        oip.erase(oip.begin());
+                    }
+                }
+                candidates.push_back(stoi(numasstring));
+            }
+        }
+        sort(candidates.begin(),candidates.end());
+        reverse(candidates.begin(),candidates.end());
+        return candidates[0];
+    }
+};
