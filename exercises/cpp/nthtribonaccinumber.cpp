a25871fc5b4d4aea07d6951d060b770f452a8b94
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-23 18:29:19 -0400

    Update nthtribonaccinumber.cpp

--- exercises/cpp/nthtribonaccinumber.cpp
+++ exercises/cpp/nthtribonaccinumber.cpp
@@ -1,3 +1,4 @@
+/* SOLUTION 1 */ 
 class Solution {
 public:
     int tribonacci(int n) {
@@ -8,5 +9,34 @@ public:
       for (int i = 3; i <= n; i++)
         res.push_back(res[i-1] + res[i-2] + res[i-3]);
       return res[n];
+    }
+};
+
+/* SOLUTION 2 */
+/* All tests passed  */
+class Solution {
+public:
+    int tribonacci(int n) 
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            vector<int> container(n+1);
+            container[0] = 0;
+            container[1] = 1;
+            container[2] = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                container[i] = container[i-1] + container[i-2] + container[i-3];
+            }
+            return container[n];
+        }
     }
 };
