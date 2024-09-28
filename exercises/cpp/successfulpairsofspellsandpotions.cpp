16171ed842e6c46431e80f800b962c52f91701f0
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-28 14:43:08 -0400

    Update successfulpairsofspellsandpotions.cpp

--- exercises/cpp/successfulpairsofspellsandpotions.cpp
+++ exercises/cpp/successfulpairsofspellsandpotions.cpp
@@ -43,3 +43,56 @@ public:
         return result;     
     }
 };
+
+/* SOLUTION 2 */ 
+/* RUNTIME ERROR ON CASE 48 of 56 */
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) 
+    {
+        vector<int> result;
+        sort(potions.begin(), potions.end());
+
+        for (int i = 0; i < spells.size(); i++)
+        {
+            int lo = 0;
+            int hi = potions.size() - 1;
+            bool elempushed = false;
+            while (lo < hi)
+            {
+                int mid = lo + (hi-lo)/2;
+                long long product = spells[i] * potions[mid];
+                if (product >= success)
+                {
+                    while (mid-1 >= 0 && spells[i]*potions[mid-1] >= success)
+                    {
+                        product = spells[i]*potions[mid-1];
+                        mid = mid-1;
+                    }
+                    result.push_back(hi - mid + 1);
+                    elempushed = true;
+                    break;
+                }
+                else if (product < success) 
+                {
+                    lo = mid + 1; 
+                }
+            }
+
+            if (elempushed == false)
+            {
+                if (potions[hi] * spells[i] >= success)
+                {
+                    result.push_back(1);
+                }
+                else
+                {
+                    result.push_back(0);
+                }
+            }
+            elempushed = false;
+        }
+
+        return result;     
+    }
+};
