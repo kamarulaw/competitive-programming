1671882f61552247ef0efb7c37713f26e8ba82a0
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-29 15:59:54 -0400

    Update equalrowsandcolumnpairs.cpp

--- exercises/cpp/equalrowsandcolumnpairs.cpp
+++ exercises/cpp/equalrowsandcolumnpairs.cpp
@@ -49,3 +49,57 @@ public:
         return ercps;
     }
 };
+
+/* SOLUTION 2 */ 
+/* All tests passed */
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) 
+    {
+        int n = grid.size();
+        map<string,int> m;
+        map<string,int> l;
+        for (int i = 0; i < n; i++)
+        {
+            string scrow = "";
+            vector<int> crow = grid[i];
+            for (int j = 0; j < n; j++)
+            {
+                scrow += to_string(crow[j]);
+                if (j != n-1)
+                {
+                    scrow += "|";
+                }
+            }
+            m[scrow]++;
+        }
+        for (int i = 0; i < n; i++) 
+        {
+            string sccol = "";
+            vector<int> ccol;
+            for (int j = 0; j < n; j++) 
+            {
+                ccol.push_back(grid[j][i]);
+            }
+            for (int j = 0; j < n; j++)
+            {
+                sccol += to_string(ccol[j]);
+                if (j != n-1)
+                {
+                    sccol += "|";
+                }
+            }
+            l[sccol]++;
+        }
+
+        int ercps = 0;    
+        for (auto elem: m)
+        {
+            if (elem.second > 0 && l[elem.first] > 0)
+            {
+                ercps += elem.second * l[elem.first];
+            }
+        }
+        return ercps;
+    }
+};
