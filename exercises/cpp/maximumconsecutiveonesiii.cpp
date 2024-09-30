9a150a8b818124496ab96dbddba02673516f9b82
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-30 11:44:07 -0400

    Update maximumconsecutiveonesiii.cpp

--- exercises/cpp/maximumconsecutiveonesiii.cpp
+++ exercises/cpp/maximumconsecutiveonesiii.cpp
@@ -36,3 +36,48 @@ public:
         return mco;
     }
 };
+
+/* SOLUTION 2 */ 
+/* All tests passed */
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int mco = 0;
+
+        int n = nums.size(); 
+
+        for (int i = 0; i < n; i++)
+        {
+            int k_ = k; 
+            int i_ = i;
+            int cco = 0;
+
+            if (mco > n - i + 1)
+            {
+                break;
+            }
+
+            while (i_ < n)
+            {
+                if (nums[i_] == 0 && k_ > 0)
+                {
+                    k_--;
+                    i_++;
+                    cco++;
+                }
+                else if (nums[i_] == 1)
+                {
+                    i_++;
+                    cco++;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            mco = max(mco, cco);
+        }
+        return mco;
+    }
+};
