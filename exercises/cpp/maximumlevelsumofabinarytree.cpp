5d63194ed50ac81a8be4e54ccc06b7ecf462baed
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-09-30 12:59:47 -0400

    Update maximumlevelsumofabinarytree.cpp

--- exercises/cpp/maximumlevelsumofabinarytree.cpp
+++ exercises/cpp/maximumlevelsumofabinarytree.cpp
@@ -1,5 +1,6 @@
 /* SOLUTION 1 */ 
 /* WRONG ANSWER ON CASE 39 of 41 */ 
+/* 8/30/21 */
 class Solution {
 public:
     map <int, vector<long long int>> m;
@@ -42,7 +43,72 @@ public:
 };
 
 /* SOLUTION 2 */ 
-/* psf */ 
+/* All tests passed */ 
+/* 9/30/24 [SOLUTION 1] Edit: Changed comparator logic within else clause*/
+class Solution {
+public:
+    map <int, vector<long long int>> m;
+    static bool comp1(vector<long long int> a, vector<long long int> b) { 
+      if (a[1] < b[1]) 
+      { 
+        return true;
+      } 
+      else if (a[1] > b[1]) 
+      { 
+        return false; 
+      } 
+      else 
+      { 
+        if (a[0] < b[0]) 
+        { 
+            return false;
+        } 
+        else if (a[0] > b[0]) 
+        { 
+          return true;
+        } 
+        else
+        {
+            return false; 
+        }
+      }
+    }
+  
+    void traversal(TreeNode* root, int depth) { 
+      if (root != NULL) 
+      { 
+        m[depth].push_back(root->val);
+        traversal(root->left, depth+1);
+        traversal(root->right, depth+1);
+      }
+    }
+  
+    int maxLevelSum(TreeNode* root) {
+      traversal(root, 1);
+      vector<vector<long long int>> sums; 
+      for (auto it: m) 
+      { 
+        long long int sum = 0; 
+        vector<long long int> arr = it.second; 
+        for (int i = 0; i < it.second.size(); i++) 
+        {
+          cout << it.second[i] << " "; // debug print line
+          sum += it.second[i];
+        }
+        cout << " <> " << sum << endl; // debug print line
+        vector<long long int> vec = {it.first, sum};
+        sums.push_back(vec);
+        sum = 0; 
+      }
+
+      sort(sums.begin(), sums.end(), comp1);
+      return sums[sums.size()-1][0];
+    }
+};
+
+/* SOLUTION 3 */ 
+/* psf */
+/* 9/29/24 */
 class Solution {
 public:
     vector<int> levelsums;
