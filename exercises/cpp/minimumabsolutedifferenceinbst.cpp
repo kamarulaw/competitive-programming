28726d839fee0c4d7e2ba8ef137df393f4f3a432
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-05 14:05:35 -0400

    Create minimumabsolutedifferenceinbst.cpp

---
+++ exercises/cpp/minimumabsolutedifferenceinbst.cpp
@@ -0,0 +1,37 @@
+/* SOLUTION 1 */ 
+/* All tests passed */
+class Solution {
+public:
+    vector<int> vals;
+
+    void traversal(TreeNode* root)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+
+        vals.push_back(root->val);
+
+        traversal(root->left);
+        traversal(root->right);
+    }
+    int getMinimumDifference(TreeNode* root) 
+    {
+        if (root == NULL)
+        {
+            return 0;
+        }
+        traversal(root);
+
+        sort(vals.begin(), vals.end());
+
+        int minabsdiff = INT_MAX;
+        for (int i = 0; i < vals.size() - 1; i++)
+        {
+            minabsdiff = min(minabsdiff, vals[i+1] - vals[i]);
+        }
+
+        return minabsdiff; 
+    }
+};
