3c4a6097257e325196a3ecf16e2c168250e08396
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-07 11:31:51 -0400

    Update rottingoranges.cpp

--- exercises/cpp/rottingoranges.cpp
+++ exercises/cpp/rottingoranges.cpp
@@ -133,3 +133,90 @@ public:
         return m*n;
     }
 };
+
+/* SOLUTION 2 */ 
+/* PRE-TESTS PASSED */
+class Solution {
+public:
+    bool inbounds(int m, int n, int x, int y)
+    {
+        if (x < 0 || x >= m)
+        {
+            return false;
+        }
+
+        if (y < 0 || y >= n)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    int orangesRotting(vector<vector<int>>& grid) 
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+                
+        int onecount = 0;
+        vector<int> start;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    onecount++;
+                }
+
+                if (grid[i][j] == 2 && start.size() == 0)
+                {
+                    start.push_back(i);
+                    start.push_back(j);
+                }
+            }
+        }
+        
+        if (onecount == 0)
+        {
+            return 0;
+        }
+        
+        int timeelapsed = 0; 
+        queue<vector<int>> q;
+        start.push_back(timeelapsed);
+        q.push(start); 
+        vector<vector<int>> dirs = {{-1,0},{1,0},{0,1},{0,-1}};     
+
+        while (q.size() > 0)
+        {
+            vector<int> front = q.front();
+            q.pop();
+            for (auto d: dirs)
+            {
+                int newx = front[0] + d[0]; 
+                int newy = front[1] + d[1];
+                if (inbounds(m,n,newx,newy) && grid[newx][newy] == 1)
+                {
+                    grid[newx][newy] = 2;
+                    onecount--;
+                    if (onecount == 0)
+                    {
+                        return front[2]+1;
+                    }
+                    vector<int> coord;
+                    coord.push_back(newx);
+                    coord.push_back(newy);
+                    coord.push_back(front[2]+1);
+                    q.push(coord);
+                }
+            }
+        }
+        
+        if (onecount > 0)
+        {
+            return -1;
+        }
+        return m*n;
+    }
+};
