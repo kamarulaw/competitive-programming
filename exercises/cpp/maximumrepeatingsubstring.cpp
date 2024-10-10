5c990823cfd615894cad56a08a7648f340e2d949
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-10 16:03:01 -0400

    Update maximumrepeatingsubstring.cpp

--- exercises/cpp/maximumrepeatingsubstring.cpp
+++ exercises/cpp/maximumrepeatingsubstring.cpp
@@ -28,3 +28,45 @@ public:
         return MRS_;
     }
 };
+
+/* SOLUTION 2 */ 
+/* psf */
+class Solution {
+public:
+    int maxRepeating(string sequence, string word) 
+    {
+        int n = sequence.length();
+        int wordlen = word.length();
+        vector<int> dp(n,0);
+
+        if (n < wordlen)
+        {
+            return 0;
+        }
+
+        for (int index = 0; index < n; index++)
+        {
+            int postindex = 0;
+            while (postindex + wordlen <= n)
+            {
+                if (sequence.substr(postindex,wordlen).compare(word) == 0)
+                {
+                    dp[postindex + wordlen] = 1 + dp[postindex];
+                    postindex = postindex + wordlen;
+                }
+                else 
+                {
+                    break;
+                }
+            }
+        }
+
+        int MRS_ = INT_MIN;
+        for (int i = 0; i < n; i++)
+        {
+            MRS_ = max(MRS_, dp[i]);
+        }
+
+        return MRS_;
+    }
+};
