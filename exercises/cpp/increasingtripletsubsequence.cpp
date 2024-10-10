e62e121fe34b7cfd1104b5ce71ab77bf35d00308
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-10 13:47:14 -0400

    Update increasingtripletsubsequence.cpp

--- exercises/cpp/increasingtripletsubsequence.cpp
+++ exercises/cpp/increasingtripletsubsequence.cpp
@@ -75,3 +75,39 @@ public:
         return false;
     }
 };
+
+/* SOLUTION 2 */ 
+/* TIME LIMIT EXCEEDED ON CASE 75 of 84 */
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) 
+    {
+        int n = nums.size();
+        
+        vector<int> LIS;
+
+        for (int i = 0; i < n; i++)
+        {
+            LIS.push_back(1);
+        }
+
+        for (int index = 1; index < n; index++)
+        {
+            for (int pre_index = 0; pre_index < index; pre_index++)
+            {
+                if (nums[index] > nums[pre_index] && LIS[index] < 1 + LIS[pre_index])
+                {
+                    LIS[index] = 1 + LIS[pre_index];
+                }
+            }
+        }
+
+        int LIS_ = INT_MIN;
+        for (int i = 0; i < n; i++)
+        {
+            LIS_ = max(LIS_,LIS[i]);
+        }
+
+        return LIS_ >= 3;
+    }
+};
