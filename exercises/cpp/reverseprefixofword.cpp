384e90a3e4d374a959768bf99af2cf4ab46087c0
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-12 16:09:32 -0400

    Create reverseprefixofword.cpp

---
+++ exercises/cpp/reverseprefixofword.cpp
@@ -0,0 +1,44 @@
+/* SOLUTION 1 */ 
+/* All tests passed */
+class Solution {
+public:
+    string reversePrefix(string word, char ch) 
+    {
+        int n = word.length();
+        int ch_ind = INT_MIN;
+        stack<char> chars_up_to_ch;
+        for (int i = 0; i < n; i++)
+        {
+            if (word[i] != ch)
+            {
+                chars_up_to_ch.push(word[i]);
+            }
+            else if (word[i] == ch)
+            {
+                chars_up_to_ch.push(word[i]);
+                ch_ind = i;
+                break;
+            }
+        }
+
+        if (ch_ind == INT_MIN)
+        {
+            return word;
+        }
+
+        string result = "";
+
+        for (int i = 0; i <= ch_ind; i++)
+        {
+            result += chars_up_to_ch.top();
+            chars_up_to_ch.pop();
+        }    
+
+        for (int i = ch_ind+1; i < n; i++)
+        {
+            result += word[i];
+        }
+
+        return result;
+    }
+};
