d516b3ea3aae527979f49e9389074be25aa38484
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-12 16:22:12 -0400

    Create cleardigits.cpp

---
+++ exercises/cpp/cleardigits.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool chardig(char c)
+    {
+        return c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9'||c=='0';
+    }
+
+    string clearDigits(string s) 
+    {
+        stack<int> chars; 
+        
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (!chardig(s[i]))
+            {
+                chars.push(s[i]);
+            }
+            else 
+            {
+                chars.pop();
+            }
+        }
+
+        string result = "";
+
+        while (chars.size() > 0)
+        {
+            result += chars.top();
+            chars.pop();
+        }
+
+        int n = result.length();
+        for (int i = 0; i < n / 2; i++)
+        {
+            char tmp = result[n-i-1];
+            result[n-i-1] = result[i]; 
+            result[i] = tmp;
+        }
+
+        return result;
+    }
+};
