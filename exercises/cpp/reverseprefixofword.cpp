2a079ca2467563ff9447b439a74c0c86cb09bbe1
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-14 16:54:16 -0400

    Update reverseprefixofword.cpp

--- exercises/cpp/reverseprefixofword.cpp
+++ exercises/cpp/reverseprefixofword.cpp
@@ -42,3 +42,44 @@ public:
         return result;
     }
 };
+
+/* SOLUTION 1 RE-FACTORED */ 
+/* ? */
+class Solution {
+public:
+    string reversePrefix(string word, char ch) 
+    {
+        int n = word.length();
+        int ch_ind = INT_MIN;
+        stack<char> chars_up_to_ch;
+        for (int i = 0; i < n; i++)
+        {
+            chars_up_to_ch.push(word[i]);
+            if (word[i] == ch)
+            {
+                ch_ind = i;
+                break;
+            }
+        }
+
+        if (ch_ind == INT_MIN)
+        {
+            return word;
+        }
+
+        string result = "";
+
+        for (int i = 0; i <= ch_ind; i++)
+        {
+            result += chars_up_to_ch.top();
+            chars_up_to_ch.pop();
+        }    
+
+        for (int i = ch_ind+1; i < n; i++)
+        {
+            result += word[i];
+        }
+
+        return result;
+    }
+};
