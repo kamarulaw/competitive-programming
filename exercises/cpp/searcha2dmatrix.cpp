e9be7d44222b1e261a068071ad45a7dc8826be76
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-17 12:47:39 -0400

    Create searcha2dmatrix.cpp

---
+++ exercises/cpp/searcha2dmatrix.cpp
@@ -0,0 +1,39 @@
+/* SOLUTION 1 */ 
+/* All tests passed */
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        int a = matrix.size();
+        int b = matrix[0].size();
+        int n = a*b;
+        int lo = 0;
+        int hi = n-1;
+        while(lo <= hi) // "lo < hi" -> "lo <= hi" took a bit of time to pick up on
+        {
+            // 9 = 4*2 + 1
+            // 5 = 4*1 + 1
+            // 6 = 4*1 + 2
+            // 7 = 4*1 + 3
+            // 8 = 4*2 + 0
+            // 11 = 4*2 + 3
+            int mid = lo + (hi - lo) / 2;
+            int row = mid / b;
+            int col = mid % b;
+            cout << mid << " | " << row << " " << col << endl; 
+            if (matrix[row][col] == target)
+            {
+                return true;
+            }
+            else if (matrix[row][col] < target)
+            {
+                lo = mid+1;
+            }
+            else 
+            {
+                hi = mid-1;
+            }
+        }
+        return false;
+    }
+};
