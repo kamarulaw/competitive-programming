93f10bf04f8fb33e50e126730a71c33f5825c7d7
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-18 14:35:48 -0400

    Update searcha2dmatrixii.cpp

--- exercises/cpp/searcha2dmatrixii.cpp
+++ exercises/cpp/searcha2dmatrixii.cpp
@@ -38,3 +38,59 @@ public:
         return false;
     }
 };
+
+/* SOLUTION 2 */ 
+/* psf */
+class Solution {
+public:
+    bool bs(vector<int> arr, int target)
+    {
+        int n = arr.size(); 
+        int lo = 0; 
+        int hi = n-1;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] == target)
+            {
+                return true;
+            }
+            else if (arr[mid] < target)
+            {
+                lo = mid + 1; 
+            }
+            else 
+            {
+                hi = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        for (int row = 0; row < matrix.size(); row++)
+        {
+            vector<int> r(matrix[row].begin()+row, matrix[row].begin()+matrix[0].size()); 
+            cout << row << endl;
+            cout << r.size() << endl;
+            if (r.size() > 0 && bs(r,target))
+            {
+                return true;
+            }
+
+            vector<int> col;
+            for (int colelem = row; colelem < matrix.size(); colelem++)
+            {
+                col.push_back(matrix[colelem][row]);
+            }
+            cout << col.size() << endl << endl;
+
+            if (col.size() > 0 && bs(col,target))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
