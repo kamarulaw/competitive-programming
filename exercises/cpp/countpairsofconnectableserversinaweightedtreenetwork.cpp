3cb86f39d0474880c5b52d7a67defe461e7169f6
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-19 11:50:27 -0400

    Update countpairsofconnectableserversinaweightedtreenetwork.cpp

--- exercises/cpp/countpairsofconnectableserversinaweightedtreenetwork.cpp
+++ exercises/cpp/countpairsofconnectableserversinaweightedtreenetwork.cpp
@@ -1,7 +1,34 @@
-/* SOLUTION 1 */ 
-/* psf */
 class Solution {
 public:
+    vector<int> result;
+    
+    void initializeResult(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            result.push_back(0);
+        }
+        return;
+    }
+
+    void traversal(int src, int dst, vector<vector<int>> graph, vector<bool> vis, map<string,int> wm, int n, string path)
+    {
+        vis[src] = true;
+        if (src == dst)
+        {
+            path += " ";
+            path += to_string(dst);
+            cout << path << endl;
+            return;
+        }
+        for (int i = 0; i < graph[src].size(); i++)
+        {
+            path += " ";
+            path += to_string(graph[src][i]);
+            traversal(graph[src][i],dst,graph,vis,wm,n,path);
+        }
+    }
+
     vector<int> countPairsOfConnectableServers(vector<vector<int>>& edges, int signalSpeed) 
     {
         int n = INT_MIN;
@@ -12,11 +39,26 @@ public:
         n++;
         vector<bool> visited(n,false);
         vector<int> adjlist;   
+        map<string,int> weightmap;
         vector<vector<int>> graph(n,adjlist);
         for (int i = 0; i < edges.size(); i++)
         {
-            graph[edges[0]].push_back(edges[1]);
-            graph[edges[1]].push_back(edges[0]);   
+            string e0e1 = ""; 
+            e0e1 += to_string(edges[i][0]); e0e1 += " "; e0e1 += to_string(edges[i][1]);
+            string e1e0 = "";
+            e1e0 += to_string(edges[i][1]); e1e0 += " "; e1e0 += to_string(edges[i][0]);
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);   
+            
         }
+        initializeResult(n);
+        for (int i = 0; i < n-1; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                traversal(i,j,graph,visited,weightmap,n,to_string(i));
+            }
+        }
+        return result;
     }
 };
