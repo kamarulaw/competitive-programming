224d923573e6ce6d00224104258d6cb03363f87c
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-19 12:03:47 -0400

    Update countpairsofconnectableserversinaweightedtreenetwork.cpp

--- exercises/cpp/countpairsofconnectableserversinaweightedtreenetwork.cpp
+++ exercises/cpp/countpairsofconnectableserversinaweightedtreenetwork.cpp
@@ -1,3 +1,5 @@
+/* SOLUTION 1 */ 
+/* psf */
 class Solution {
 public:
     vector<int> result;
@@ -59,6 +61,81 @@ public:
                 traversal(i,j,graph,visited,weightmap,n,to_string(i));
             }
         }
+        return result;
+    }
+};
+
+/* SOLUTION 2 */ 
+/* psf */
+class Solution {
+public:
+    vector<int> result;
+    
+    void initializeResult(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            result.push_back(0);
+        }
+        return;
+    }
+
+    void traversal(int src, int dst, vector<vector<int>> graph, vector<bool> vis, map<string,int> wm, int n, string path)
+    {
+        vis[src] = true;
+
+        if (src == dst)
+        {
+            path += " ";
+            path += to_string(dst);
+            cout << path << endl;
+            return;
+        }
+
+        for (int i = 0; i < graph[src].size(); i++)
+        {
+            path += " ";
+            path += to_string(graph[src][i]);
+            traversal(graph[src][i],dst,graph,vis,wm,n,path);
+        }
+    }
+
+    vector<int> countPairsOfConnectableServers(vector<vector<int>>& edges, int signalSpeed) 
+    {
+        int n = INT_MIN;
+        for (int i = 0; i < edges.size(); i++)
+        {
+            n = max(n,max(edges[i][0],edges[i][1]));
+        }
+        n++;
+
+        int[n][n] graph;
+        bool[n] visited;
+        for (int i = 0; i < n; i++)
+        {
+            visited[i] = true;
+            for (int j = 0; j < n; j++)
+            {
+                graph[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < edges.size(); i++)
+        {
+            graph[edges[i][1]][edges[i][0]] = edges[i][2];
+            graph[edges[i][0]][edges[i][1]] = edges[i][2];
+            
+        }
+
+        initializeResult(n);
+        for (int i = 0; i < n-1; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                traversal(i,j,graph,visited,n,to_string(i));
+            }
+        }
+
         return result;
     }
 };
