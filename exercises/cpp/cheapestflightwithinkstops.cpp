9eb86eb2515d7f6f10e581999d83affcceff4241
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-22 15:05:37 -0400

    Update cheapestflightwithinkstops.cpp

--- exercises/cpp/cheapestflightwithinkstops.cpp
+++ exercises/cpp/cheapestflightwithinkstops.cpp
@@ -70,3 +70,79 @@ public:
         return -1;
     }
 };
+
+/* SOLUTION 2 */ 
+/* psf */
+class Solution {
+public:
+    vector<int> relevant_prices;
+
+    void dfs(int src, int dst, vector<vector<int>> g, map<string,int> m, vector<bool> visited, int n, int k)
+    {
+        visited[src] = true;
+        pair<vector<int>,vector<bool>> start;
+        start.first = {}; 
+        start.first.push_back(src);
+        start.first.push_back(-1);
+        start.first.push_back(0);
+        start.second = visited;
+        stack<pair<vector<int>,vector<bool>>> stack_;
+        stack_.push(start);
+        while (stack_.size() > 0)
+        {
+            int node = stack_.top().first[0]; 
+            int stops = stack_.top().first[1]; 
+            int price = stack_.top().first[2];
+            
+            vector<bool> vis = stack_.top().second;
+            stack_.pop();
+            string m_str = to_string(node);
+            m_str += " ";
+            for (int i = 0; i < g[node].size(); i++)
+            {    
+                if (!vis[g[node][i]])
+                {
+                    vis[g[node][i]] = true;
+                    m_str += to_string(g[node][i]);
+                    stops++;
+                    price += m[m_str];
+                    if (stops <= k && g[node][i] == dst)
+                    {
+                        relevant_prices.push_back(price);
+                    }
+                    pair<vector<int>,vector<bool>> newtop;
+                    newtop.first = {};
+                    newtop.first.push_back(g[node][i]);
+                    newtop.first.push_back(stops);
+                    newtop.first.push_back(price);
+                    newtop.second = vis;
+                    stack_.push(newtop);
+                }
+            }
+        }
+        return;
+    }
+
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+        vector<int> v;
+        map<string,int> m;
+        vector<vector<int>> g(n,v);
+        vector<bool> visited(n,false);   
+        for (int i = 0; i < flights.size(); i++)
+        {
+            string src_dst_str = to_string(flights[i][0]);
+            src_dst_str += " "; 
+            src_dst_str += to_string(flights[i][1]);
+            g[flights[i][0]].push_back(flights[i][1]);
+            m[src_dst_str] = flights[i][2];
+        }
+        dfs(src, dst, g, m, visited, n, k);
+        if (relevant_prices.size() > 0)
+        {
+            sort(relevant_prices.begin(),relevant_prices.end());
+            return relevant_prices[0];
+        }
+        return -1;
+    }
+};
