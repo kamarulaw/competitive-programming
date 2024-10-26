fc64bfc3e4f20ce8cedafc5e74ec2be417322ba3
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-26 11:37:53 -0400

    Create largestnumberatleasttwiceofothers.cpp

---
+++ largestnumberatleasttwiceofothers.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) 
+    {
+        int elem; vector<int> nums_ = nums; sort(nums_.begin(),nums_.end()); 
+        if (nums_[nums_.size()-1] >= 2*nums_[nums_.size()-2]){elem = nums_[nums_.size()-1]; for (int i = 0; i < nums.size(); i++) { if (nums[i] == elem) {return i;}}}return -1;
+    }
+};
