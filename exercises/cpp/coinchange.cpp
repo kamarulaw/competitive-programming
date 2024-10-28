df652f87850b4490c8b749243e7cc056c419e38e
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-10-28 14:53:21 -0400

    Update coinchange.cpp

--- exercises/cpp/coinchange.cpp
+++ exercises/cpp/coinchange.cpp
@@ -38,3 +38,63 @@ public:
         }
     }
 };
+
+/* SOLUTION 2 */ 
+/* psf */
+class Solution {
+public:
+    vector<int> coins_;
+    vector<int> sol;
+    int fewestnumcoins = INT_MAX;
+    
+    void coinchangeutil(int numcoins, int amount, bool startflag)
+    {
+        if (startflag == true)
+        {
+            sol.clear();
+        }
+        startflag = false;
+
+        if (amount == 0)
+        {
+            sol.push_back(numcoins);
+            return;
+        }
+        else if (amount < 0)
+        {
+            return;
+        }
+        else 
+        {
+            for (int i = 0; i < coins_.size(); i++)
+            {
+                coinchangeutil(numcoins+1, amount - coins_[i], false);
+            }
+        }
+    }
+
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        int n = coins.size();
+        coins_ = coins;
+        sort(coins_.begin(), coins_.end());
+        reverse(coins_.begin(), coins_.end());
+        vector<int> dp(n+1,INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if (dp[i-j] != INT_MAX)
+                {
+                    coinchangeutil(dp[i-j],amount-i+j,true);
+                    if (sol.size() > 0)
+                    {
+                        dp[i-j] = sol[0];
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+};
