01a876811a1fdb18fc811218135c92b6ce2e7e68
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-01 17:43:28 -0400

    Create plusone.cpp

---
+++ exercises/cpp/plusone.cpp
@@ -0,0 +1,35 @@
+/* SOLUTION 1 */ 
+/* psf */
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) 
+    {
+        int n = digits.size();
+        if (digits[n-1] != 9)
+        {
+            digits[n-1]++; 
+            return digits;
+        }
+        else
+        {
+            int k = n-1;
+            while (digits[k] == 9)
+            {
+                digits[k] == 0;
+                k--;
+            } 
+
+            if (k >= 0)
+            {
+                digits[k]++;
+                return digits;
+            }
+            else 
+            {
+                digits.insert(digits.begin(),1);
+                return digits;
+            }
+           
+        }
+    }
+};
