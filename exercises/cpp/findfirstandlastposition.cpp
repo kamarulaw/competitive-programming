9449d7713a0c349b39c99487584a3851029cfb11
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-02 13:56:06 -0400

    Create findfirstandlastposition.cpp

---
+++ exercises/cpp/findfirstandlastposition.cpp
@@ -0,0 +1,46 @@
+/* SOLUTION 1 */ 
+/* psf */
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) 
+    {
+        vector<int> ans;
+        int lo = 0; 
+        int hi = nums.size()-1;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo)/2; 
+            if (nums[mid] == target)
+            {
+                int midl = mid;
+                int midh = mid;
+                while (midl-1 >= 0 && nums[midl-1] == target) 
+                {
+                    midl = midl-1; 
+                }
+                ans.push_back(midl);
+
+                while (midh+1 < nums.size() && nums[midh+1] == target)
+                {
+                    midh = midh + 1; 
+                }
+                ans.push_back(midh);
+            }
+            else if (nums[mid] < target)
+            {
+                lo = mid + 1; 
+            }
+            else 
+            {
+                hi = mid - 1;
+            }
+        }
+        
+        if (ans.size() == 0)
+        {
+            return {-1,-1};
+        }
+        return ans; 
+    }
+};
+// 
