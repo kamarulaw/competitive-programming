4e2782dbf981dcef4a2571583bcdfe82b4bf71fc
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-04 11:55:23 -0500

    Create longestsubsequencewithlimitedsum.cpp

---
+++ exercises/cpp/longestsubsequencewithlimitedsum.cpp
@@ -0,0 +1,41 @@
+/* SOLUTION 1 */
+/* WRONG ANSWER ON CASE 56 of 57 */
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) 
+    {
+        int n = nums.size(); 
+        int m = queries.size();
+        vector<int> answer(m,0);
+        sort(nums.begin(),nums.end());
+        for (int i = 0; i < m; i++)
+        {
+            int sum = 0;
+            int numsind = 0;
+            while (numsind < n)
+            {
+                if (sum < queries[i])
+                {
+                    sum += nums[numsind];
+                    numsind++;
+                }
+                if (sum == queries[i])
+                {
+                    answer[i] = numsind;
+                    break;
+                }
+                if (sum > queries[i])
+                {
+                    answer[i] = numsind - 1;
+                    break;
+                }
+                
+                if (sum < queries[i] && numsind == n-1)
+                {
+                    answer[i] = n;
+                }
+            }
+        }
+        return answer;
+    }
+};
