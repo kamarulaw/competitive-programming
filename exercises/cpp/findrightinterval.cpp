cd73e0d6ebc99d32d54d4355aa72792fab66eeb8
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-05 11:25:12 -0500

    Create findrightinterval.cpp

---
+++ exercises/cpp/findrightinterval.cpp
@@ -0,0 +1,36 @@
+/* SOLUTION 1 */
+/* WRONG ANSWER ON CASE 18 of 20 */
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) 
+    {
+        int n = intervals.size();
+        vector<int> ans(n,INT_MAX);
+        for (int i = 0; i < intervals.size(); i++)
+        {
+            int minintervalstart = INT_MAX; 
+            int intervalnumber = INT_MAX;
+            for (int j = 0; j < intervals.size(); j++)
+            {
+                if (j == i)
+                {
+                    continue;
+                }
+                if (intervals[j][0] >= intervals[i][1] && intervals[j][0] < minintervalstart)
+                {
+                    minintervalstart = intervals[j][0];
+                    intervalnumber = j;
+                }
+            }
+            if (intervalnumber != INT_MAX)
+            {
+                ans[i] = intervalnumber;
+            }
+            else
+            {
+                ans[i] = -1;
+            }
+        }    
+        return ans;
+    }
+};
