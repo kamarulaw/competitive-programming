2ef8f862c308ebc49a7365c55c007b54d5eb7ae8
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-06 11:25:13 -0500

    Update findtheduplicatenumber.cpp

--- exercises/cpp/findtheduplicatenumber.cpp
+++ exercises/cpp/findtheduplicatenumber.cpp
@@ -28,3 +28,33 @@ public:
         return -1;
     }
 };
+
+/* SOLUTION 2 */ 
+/* All tests passed */
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) 
+    {
+        /*
+        // if repeated number only comes up once
+        int n = nums.size() - 1;
+        int total = 0;
+        for (int i = 0; i < n + 1; i++)
+        {
+            total += nums[i];
+        }
+        return total - ((n)*(n+1)/2);    
+        */
+        int n = nums.size();
+        map<int,int> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[nums[i]]++; 
+            if (m[nums[i]] > 1)
+            {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
