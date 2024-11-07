0117f4bf52bb1746f5a4f3c6d54af7beb62820ca
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-07 10:18:17 -0500

    Update removeelement.cpp

--- exercises/cpp/removeelement.cpp
+++ exercises/cpp/removeelement.cpp
@@ -76,6 +76,48 @@ public:
     }
 };
 
+/* SOLUTION 4 */
+/* this solution is correct, and isnt being recognized as correct */
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) 
+    {
+        int n = nums.size();
+        int count = 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == val)
+            {
+                count++;
+            }
+        }
+        for (int i = 0; i < n - count; i++)
+        {
+            int badindloc = n-1;
+            if (nums[i] == val)
+            {
+                while (badindloc >= 0 && nums[badindloc] == val)
+                {
+                    badindloc--;
+                }
+                int temp = nums[badindloc];
+                nums[badindloc] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        /*
+        for (int i = 0; i < n; i++)
+        {
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+        */
+        return count;    
+    }
+};
+
+
 /*
 //onlinegdb debug
 #include <iostream>
