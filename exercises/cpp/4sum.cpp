72a989c51823d5b9963d23eba10ccdaba6269bf9
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-08 14:33:28 -0500

    Update 4sum.cpp

--- exercises/cpp/4sum.cpp
+++ exercises/cpp/4sum.cpp
@@ -61,3 +61,44 @@ public:
         return vanswer;    
     }
 };
+
+/* SOLUTION 3 */
+/* TIME LIMIT EXCEEDED ON CASE 249 of 294 */ 
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) 
+    {
+        map<int,int> m;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            m[nums[i]]++;
+        } 
+        set<vector<int>> answer;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = 0; j < nums.size(); j++)
+            {
+                for (int k = 0; k < nums.size(); k++)
+                {
+                    if (i != j && j != k && i != k)
+                    {
+                        m[nums[i]]--; 
+                        m[nums[j]]--;
+                        m[nums[k]]--;
+                        if (m[target-nums[i]-nums[j]-nums[k]] >= 1)
+                        {
+                            vector<int> ss = {nums[i],nums[j],nums[k],target-nums[i]-nums[j]-nums[k]};
+                            sort(ss.begin(),ss.end());
+                            answer.emplace(ss);
+                        }
+                        m[nums[i]]++;
+                        m[nums[j]]++;
+                        m[nums[k]]++;
+                    }
+                }
+            }
+        } 
+        vector<vector<int>> vanswer(answer.begin(),answer.end());
+        return vanswer;  
+    }
+};
