d9a5f5229c5c8936d713264bfd3746a444c9c838
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-08 15:22:49 -0500

    Create 2sumii_inputarrayissorted.cpp

---
+++ exercises/cpp/2sumii_inputarrayissorted.cpp
@@ -0,0 +1,55 @@
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) 
+    {
+        map<int,int> m;
+        vector<int> answer;
+        int n = numbers.size();
+        for (int i = 0; i < n; i++)
+        {
+            m[numbers[i]]++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            m[numbers[i]]--;
+            if (m[target-numbers[i]] >= 1)
+            {
+                answer.push_back(i+1);
+                int lo;
+                int hi;
+                if (target - numbers[i] >= numbers[i])
+                {
+                    lo = i+1; 
+                    hi = n-1;
+                }
+                else 
+                {
+                    lo = 0;
+                    hi = i-1;
+                }
+                while (lo <= hi)
+                {
+                    int mid = lo + (hi - lo) / 2;
+                    if (numbers[mid] == target-numbers[i])
+                    {
+                        answer.push_back(mid+1); 
+                        sort(answer.begin(),answer.end()); 
+                        return answer;
+                    }
+                    else if (numbers[mid] < target-numbers[i])
+                    {
+                        lo = mid+1;
+                    }
+                    else 
+                    {
+                        hi = mid-1;
+                    }
+                }
+            }
+            m[numbers[i]]++;
+        }
+        return answer;    
+    }
+};
