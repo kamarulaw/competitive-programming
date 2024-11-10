3754f0ee201a8ca5139d45fbada68b901718509d
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-10 15:40:21 -0500

    Create minimumsubsequenceinnonincreasingorder.cpp

---
+++ exercises/cpp/minimumsubsequenceinnonincreasingorder.cpp
@@ -0,0 +1,34 @@
+/* SOLUTION 1 */
+/* WRONG ANSWER ON CASE 102 of 103 */
+class Solution {
+public:
+    vector<int> minSubsequence(vector<int>& nums) 
+    {
+        if (nums.size() == 1)
+        {
+            return nums;
+        }
+        int n = nums.size();
+        int total = 0;
+        for (int i = 0; i < n; i++)
+        {
+            total += nums[i];
+        }
+        sort(nums.begin(),nums.end());
+        for (int k = 1; k <= n-1; k++)
+        {
+            int sstotal = 0;
+            vector<int> subseq(nums.end()-k,nums.end());
+            for (int i = 0; i < subseq.size(); i++)
+            {
+                sstotal += subseq[i];
+            }
+            if (sstotal > total - sstotal)
+            {
+                reverse(subseq.begin(), subseq.end());
+                return subseq;
+            }
+        }
+        return {};
+    }
+};
