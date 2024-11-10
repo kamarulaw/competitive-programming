8400db244bee7012d226fd3d87dd3e4e9366a1d8
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-10 14:36:49 -0500

    Update lemonadechange.cpp

--- exercises/cpp/lemonadechange.cpp
+++ exercises/cpp/lemonadechange.cpp
@@ -43,3 +43,60 @@ public:
         return true;   
     }
 };
+
+/* SOLUTION 2 */
+/* psf */
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) 
+    {
+        int fivcount = 0;
+        int tencount = 0;
+        int ttycount = 0;
+        int n = bills.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (bills[i] == 5)
+            {
+                fivcount++;
+            }
+            else if (bills[i] == 10)
+            {
+                tencount++;
+                if (fivcount < 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    fivcount--;
+                }
+            }
+            else
+            {
+                ttycount++;
+                if (tencount < 1 && fivcount < 3) 
+                {
+                    return false;
+                }
+                else if (tencount < 1 || fivcount < 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (tencount >= 1 && fivcount >= 1)
+                    {
+                        fivcount--;
+                        tencount--;
+                    }
+                    else if (fivcount >= 3)
+                    {
+                        fivcount-=3;
+                    }
+                }
+            }
+        } 
+        return true;   
+    }
+};
