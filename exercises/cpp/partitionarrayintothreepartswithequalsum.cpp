1a51726c6517cedf282f8977ba20a57420fe00f7
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-10 14:48:28 -0500

    Create partitionarrayintothreepartswithequalsum.cpp

---
+++ exercises/cpp/partitionarrayintothreepartswithequalsum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& arr) 
+    {
+        // arr.size() stars && 2 bars problem
+        // Example: 5 stars && 2 bars problem
+        // *****
+        // *|*|***
+        // *|**|**
+        // *|***|*
+        // every set has to have at least one element
+        // start with |group1| = 1, |group2| = 1, |group3| = arr.size() - 2
+        // 1|1|3 -> 
+        // how many ways are there
+        
+    }
+};
