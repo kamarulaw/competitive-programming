f97d43cb7bd44961ed9889c03e34e91f9122c0f4
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-10 15:01:01 -0500

    Update partitionarrayintothreepartswithequalsum.cpp

--- exercises/cpp/partitionarrayintothreepartswithequalsum.cpp
+++ exercises/cpp/partitionarrayintothreepartswithequalsum.cpp
@@ -1,5 +1,17 @@
+/* SOLUTION 1 */
+/* psf */
 class Solution {
 public:
+    int groupsum(vector<int> vec)
+    {
+        int total = 0;
+        for (int i = 0; i < vec.size(); i++)
+        {
+            total += vec[i];
+        }
+        return total;
+    }
+
     bool canThreePartsEqualSum(vector<int>& arr) 
     {
         // arr.size() stars && 2 bars problem
@@ -11,7 +23,37 @@ public:
         // every set has to have at least one element
         // start with |group1| = 1, |group2| = 1, |group3| = arr.size() - 2
         // 1|1|3 -> 
-        // how many ways are there
-        
+        // how many ways are
+
+        int n = arr.size();
+        for (int i = 1; i <= n-2; i++)
+        {
+            for (int j = i; j <= n-1; j++)
+            {
+                for (int k = j; j <= n; j++)
+                {
+                    vector<int> group1;
+                    vector<int> group2;
+                    vector<int> group3;
+                    for (int ii = 0; ii < i; ii++)
+                    {
+                        group1.push_back(arr[ii]);
+                    }
+                    for (int jj = i; jj < j; jj++)
+                    {
+                        group2.push_back(arr[jj]);
+                    }
+                    for (int kk = j; kk < k; kk++)
+                    {
+                        group3.push_back(arr[kk]);
+                    }
+                    if (groupsum(group1) == groupsum(group3) && groupsum(group2) == groupsum(group1))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return falsse;
     }
 };
