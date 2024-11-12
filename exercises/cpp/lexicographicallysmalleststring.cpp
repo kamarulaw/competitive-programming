6cd7c8698d5fde2ddd5d1d39faeb24f945c55ed8
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-12 10:55:17 -0500

    Update lexicographicallysmalleststring.cpp

--- exercises/cpp/lexicographicallysmalleststring.cpp
+++ exercises/cpp/lexicographicallysmalleststring.cpp
@@ -42,3 +42,49 @@ public:
         return answer;    
     }
 };
+
+/* SOLUTION 2 */
+/* RUNTIME ERROR ON CASE 783 of 895 */
+class Solution {
+public:
+    string getSmallestString(string s) 
+    {
+        vector<unsigned long long int> nums;
+        int n = s.length();
+        nums.push_back(stoull(s));
+        cout << stoull(s) << endl;
+        for (int i = 0; i < n-1; i++)
+        {
+            nums.push_back(stoull(s));
+            int curr = s[i] - '0';
+            int next = s[i+1] - '0';
+            if (curr%2 == next%2)
+            {
+                char tmp = s[i];
+                s[i] = s[i+1]; 
+                s[i+1] = tmp;
+                nums.push_back(stoll(s));
+                tmp = s[i];
+                s[i] = s[i+1]; 
+                s[i+1] = tmp;
+            }
+        }
+        sort(nums.begin(),nums.end());
+        string answer = to_string(nums[0]);
+        bool reversed = false;
+        while (answer.length() < s.length())
+        {
+            if (reversed == false)
+            {
+                reverse(answer.begin(),answer.end()); 
+                reversed = true;
+            }
+            answer.push_back('0');
+        }
+        if (reversed == true)
+        {
+            reverse(answer.begin(),answer.end());
+        }
+        return answer;    
+    }
+};
