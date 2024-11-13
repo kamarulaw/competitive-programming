1ef04e92a9e110df715127932b2b2ae066f83514
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-13 11:10:38 -0500

    Update singleelementinasortedarray.cpp

--- exercises/cpp/singleelementinasortedarray.cpp
+++ exercises/cpp/singleelementinasortedarray.cpp
@@ -4,6 +4,7 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) 
     {
+        // O(log n) time, O(1) space 
         int lo = 0; 
         int hi = nums.size();
         while (lo <= hi)
