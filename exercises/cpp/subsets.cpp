bda097e7942670f50acc36424117354ae4ed3753
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-13 14:10:09 -0500

    Create subsets.cpp

---
+++ exercises/cpp/subsets.cpp
@@ -0,0 +1,28 @@
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    vector<int> nums_;
+    set<vector<int>> subsets_;
+    void subsetutil(int i, int n, vector<int> ss)
+    {
+        if (i == n)
+        {
+            sort(ss.begin(), ss.end());
+            subsets_.emplace(ss);
+            return;
+        }
+        subsetutil(i+1,n,ss);
+        ss.push_back(nums_[i]);
+        subsetutil(i+1,n,ss);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        nums_ = nums; 
+        int n = nums_.size(); 
+        subsetutil(0,n,{});
+        vector<vector<int>> vsubsets(subsets_.begin(), subsets_.end());
+        return vsubsets;
+    }
+};
