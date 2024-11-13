d1ae8965120a5a5b1c4238f697864a230d3b7730
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-13 10:58:09 -0500

    Create singleelementinasortedarray.cpp

---
+++ exercises/cpp/singleelementinasortedarray.cpp
@@ -0,0 +1,15 @@
+/* SOLUTION 1 */ 
+/* psf */
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) 
+    {
+        int lo = 0; 
+        int hi = nums.size();
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            
+        }    
+    }
+};
