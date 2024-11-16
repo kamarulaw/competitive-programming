224e912c67dad183999bc540aff6e12577ed352a
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-16 13:22:08 -0500

    Update singleelementinasortedarray.cpp

--- exercises/cpp/singleelementinasortedarray.cpp
+++ exercises/cpp/singleelementinasortedarray.cpp
@@ -6,11 +6,35 @@ public:
     {
         // O(log n) time, O(1) space 
         int lo = 0; 
-        int hi = nums.size();
-        while (lo <= hi)
+        int hi = nums.size()-1;
+        int first = nums[lo];
+        int last = nums[hi];
+        for (int elem = first; elem <= last; elem++)
         {
-            int mid = lo + (hi - lo) / 2;
-            
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (nums[mid] == elem)
+                {
+                    if (mid - 1 >= 0 && nums[mid-1] == elem)
+                    {
+                        continue;
+                    }
+                    if (mid + 1 < nums.size && nums[mid+1] == elem)
+                    {
+                        continue;
+                    }
+                    return nums[mid];
+                }
+                else if (nums[mid] < elem)
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    hi = mid - 1;
+                }
+            }
         }    
     }
 };
