73b11140e7b612747233fe71680e126be030fdb5
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-18 15:46:50 -0500

    Update xofakindinadeckofcards.cpp

--- exercises/cpp/xofakindinadeckofcards.cpp
+++ exercises/cpp/xofakindinadeckofcards.cpp
@@ -27,3 +27,81 @@ public:
         map<int,int> m; for (int i = 0; i < deck.size(); i++){m[deck[i]]++;} int gc = INT_MAX; for (auto it: m){gc = min(it.second,gc);} if (gc == 1) {return false;} for (auto it: m){if (it.second % gc != 0) {cout << it.second << endl; return false;}} return true; 
     }
 };
+
+/* SOLUTION 4 */
+/* psf */
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) 
+    {
+        map<int,int> m; 
+        for (int i = 0; i < deck.size(); i++)
+        {
+            m[deck[i]]++;
+        } 
+
+        int gc = INT_MAX; 
+
+        for (auto it: m)
+        {
+            gc = min(it.second,gc);
+        } 
+        
+        if (gc == 1) 
+        {
+            return false;
+        } 
+
+        map<int, vector<int>> count_pfs;
+
+        for (int i = 2; i <= (int)pow(gc,.5); i++)
+        {   
+            if (gc % i == 0 && isprime(i))
+            {
+                gc_primefactorization.emplace(i);
+            }
+            else 
+            {
+                continue;
+            }
+        }
+
+        vector<int> gc_pf(gc_primefactorization.begin(), gc_primefactorization.end());
+        
+        for (auto it: m)
+        {
+            if (it.second % gc != 0) 
+            {
+                return false;
+            }
+        }
+        return true; 
+    }
+
+    bool isPrime(int n)
+    {
+        if (n < 2)
+        {
+            return false;
+        }
+
+        if (n == 2)
+        {
+            return true;
+        }
+
+        if (n % 2 == 0)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= (int)pow(n, .5); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            } 
+        }
+        return true;
+    }
+};
