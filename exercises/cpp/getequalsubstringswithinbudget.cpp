9d68b89106ec0c32e224caa71b70c4f9f83a1da8
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-18 10:33:59 -0500

    Update getequalsubstringswithinbudget.cpp

--- exercises/cpp/getequalsubstringswithinbudget.cpp
+++ exercises/cpp/getequalsubstringswithinbudget.cpp
@@ -31,3 +31,35 @@ public:
         return mlen;
     }
 };
+
+/* SOLUTION 2 */
+/* TIME LIMIT EXCEEDED ON CASE 35 of 37 */
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) 
+    {
+        long long mlen = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j < n-i+1; j++)
+            {
+                long long ccost = 0;
+                string ssub = s.substr(i,j);
+                string tsub = t.substr(i,j);
+                for (int k = 0; k < ssub.length(); k++)
+                {
+                    ccost += abs((int)ssub[k] - (int)tsub[k]);
+                }
+                if (ccost <= maxCost)
+                {
+                    if ((long long)ssub.length() > mlen)
+                    {
+                        mlen = ssub.length();
+                    }
+                }
+            }
+        }
+        return mlen;
+    }
+};
