866c40d9337f6446a54859e74cd1696f36765eb5
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-18 15:18:29 -0500

    Update xofakindinadeckofcards.cpp

--- exercises/cpp/xofakindinadeckofcards.cpp
+++ exercises/cpp/xofakindinadeckofcards.cpp
@@ -17,3 +17,13 @@ public:
         map<int,int> m; for (int i = 0; i < deck.size(); i++){m[deck[i]]++;} int gc = INT_MAX; for (auto it: m){gc = min(it.second,gc);} if (gc == 1) {return false;} for (auto it: m){if (it.second % 2 != 0) {return false;}} return true; 
     }
 };
+
+/* SOLUTION 3 */
+/* WRONG ANSWER ON CASE 71 of 75 */
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) 
+    {
+        map<int,int> m; for (int i = 0; i < deck.size(); i++){m[deck[i]]++;} int gc = INT_MAX; for (auto it: m){gc = min(it.second,gc);} if (gc == 1) {return false;} for (auto it: m){if (it.second % gc != 0) {cout << it.second << endl; return false;}} return true; 
+    }
+};
