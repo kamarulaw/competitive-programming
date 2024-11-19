f38ef822b09bed0f55f4addefbc99fe75e5748cd
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-19 14:13:41 -0500

    Create powerofthree.cpp

---
+++ exercises/cpp/powerofthree.cpp
@@ -0,0 +1,17 @@
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    bool isPowerOfThree(int n) 
+    {
+        while (n > 1)
+        {
+            if (n % 3 != 0)
+            {
+                return false;
+            }
+            n /= 3;
+        }
+        return n == 1;    
+    }
+};
