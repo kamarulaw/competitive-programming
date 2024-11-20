6968d1688d799c2c44fb318cea5d17f331be0e82
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-20 12:35:43 -0500

    Update secondlargestdigitinastring.cpp

--- exercises/cpp/secondlargestdigitinastring.cpp
+++ exercises/cpp/secondlargestdigitinastring.cpp
@@ -20,3 +20,49 @@ public:
         return vdigits[1];    
     }
 };
+
+/* SOLUTION 2 */
+/* All tests passed */
+class Solution {
+public:
+    int secondHighest(string s) 
+    {
+        set<int> digits;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (0 <= s[i] - '0' && s[i] - '0' <= 9)
+            {
+                digits.emplace(s[i] - '0');
+            }
+        }
+        if (digits.size() <= 1)
+        {
+            return -1;
+        }
+        vector<int> vdigits(digits.begin(), digits.end()); sort(vdigits.begin(), vdigits.end()); 
+        return vdigits[1];    
+    }
+};
+
+/* SOLUTION 3 */
+/* All tests passed */
+class Solution {
+public:
+    int secondHighest(string s) 
+    {
+        set<int> digits;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (0 <= s[i] - '0' && s[i] - '0' <= 9)
+            {
+                digits.emplace(s[i] - '0');
+            }
+        }
+        if (digits.size() <= 1)
+        {
+            return -1;
+        }
+        vector<int> vdigits(digits.begin(), digits.end()); 
+        return vdigits[vdigits.size()-2];    
+    }
+};
