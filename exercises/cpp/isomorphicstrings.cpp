12136ae8ee0d5b16e56ca1b01afe20fd076a17a3
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-22 11:05:03 -0500

    Update isomorphicstrings.cpp

--- exercises/cpp/isomorphicstrings.cpp
+++ exercises/cpp/isomorphicstrings.cpp
@@ -1,5 +1,5 @@
 /* SOLUTION 1 */
-/* WRONG ANSWER ON CASE 38 of 48 */
+/* WRONG ANSWER ON CASE 43 of 46 */
 class Solution {
 public:
     bool isIsomorphic(string s, string t) 
@@ -41,5 +41,34 @@ public:
             }
         }
         return true;
+    }
+};
+
+/* SOLUTION 2 */
+/* WRONG ANSWER ON CASE 43 of 46 */
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) 
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+        int n = s.length();
+        map<char,int> smap;
+        map<char,int> tmap;
+        for (int i = 0; i < n; i++)
+        {
+            smap[s[i]]++;
+            tmap[t[i]]++;
+        }
+        string scountstring = "";
+        string tcountstring = "";
+        for (int i = 0; i < n; i++)
+        {
+            scountstring += to_string(smap[s[i]]);
+            tcountstring += to_string(tmap[t[i]]);
+        }
+        return scountstring == tcountstring;
     }
 };
