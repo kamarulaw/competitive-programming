30154c681acdb584f757495c10b29388a4b66c71
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-22 14:14:45 -0500

    Update countbinarysubstrings.cpp

--- exercises/cpp/countbinarysubstrings.cpp
+++ exercises/cpp/countbinarysubstrings.cpp
@@ -34,3 +34,50 @@ public:
         return (ans/2)+1;  
     }
 };
+
+/* SOLUTION 2 */
+/* psf */ 
+class Solution {
+public:
+    int countBinarySubstrings(string s) 
+    {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 2; j <= n-i; j+=2)
+            {
+                string sstr = s.substr(i,j); 
+                cout << i <<" : " << sstr << endl;
+                int oc = 0; 
+                int zc = 0;
+                bool zerosfirsthalf = true;
+                bool onesfirsthalf = true;
+                for (int k = 0; k < sstr.length(); k++)
+                {
+                    if (sstr[k] == '1')
+                    {
+                        oc++;
+                    }
+                    else 
+                    {
+                        zc++;
+                    }
+                    if (sstr[0] == '0' && k < sstr.length() / 2 && sstr[k] != '0')
+                    {
+                        zerosfirsthalf = false;
+                    }
+                    if (sstr[0] == '1' && k < sstr.length() / 2 && sstr[k] != '1')
+                    {
+                        onesfirsthalf = false;
+                    }
+                }
+                if ((oc == zc) && (zerosfirsthalf || onesfirsthalf))
+                {
+                    ans++;
+                }
+            }
+        }  
+        return ans;  
+    }
+};
