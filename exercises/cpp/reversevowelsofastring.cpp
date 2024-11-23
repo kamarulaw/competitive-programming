23434ced261e284bc9b84bbcf37a49888a868a95
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-23 13:21:01 -0500

    Update reversevowelsofastring.cpp

--- exercises/cpp/reversevowelsofastring.cpp
+++ exercises/cpp/reversevowelsofastring.cpp
@@ -34,3 +34,49 @@ public:
         return s;
     }
 };
+
+/* SOLUTION 2 */
+/* All tests passed */
+class Solution {
+public:
+    bool isvowel(char c)
+    {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
+    string reverseVowels(string s) 
+    {
+        vector<int> vowelinds;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (isvowel(s[i]))
+            {
+                vowelinds.push_back(i);
+            }
+        }
+        
+        int vowelind = 0;
+        int n = vowelinds.size();
+        for (int i = 0; i < n / 2; i++)
+        {
+            int tmp = vowelinds[i];
+            vowelinds[i] = vowelinds[n-i-1];
+            vowelinds[n-1-i] = tmp;
+        }
+        string result = "";
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (isvowel(s[i]))
+            {
+                result += s[vowelinds[vowelind]];
+                vowelind++;
+            }
+            else 
+            {
+                result += s[i];
+            }
+        }
+        return result;
+    }
+};
