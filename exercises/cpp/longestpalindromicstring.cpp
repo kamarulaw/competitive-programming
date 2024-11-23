cdbc544a20638ce02d547765fd9db9d51b9544ca
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-23 13:46:52 -0500

    Update longestpalindromicstring.cpp

--- exercises/cpp/longestpalindromicstring.cpp
+++ exercises/cpp/longestpalindromicstring.cpp
@@ -28,3 +28,35 @@ public:
         return lps;
     }
 };
+
+/* SOLUTION 2 */
+/* TIME LIMIT EXCEEDED ON CASE 90 of 142 */ 
+class Solution {
+public:
+    string longestPalindrome(string s) 
+    {
+        string lps = "";
+        int n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j <= n-i; j++)
+            {
+                string sstr = s.substr(i,j);
+                bool palindromic = true;
+                for (int k = 0; k < sstr.length()/2; k++)
+                {
+                    if (sstr[k] != sstr[sstr.length()-1-k])
+                    {
+                        palindromic = false;
+                        break;
+                    }
+                }
+                if (palindromic == true && sstr.length() > lps.length())
+                {
+                    lps = sstr;
+                }
+            }
+        }    
+        return lps;
+    }
+};
