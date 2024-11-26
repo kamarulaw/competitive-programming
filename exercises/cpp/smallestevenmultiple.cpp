c413f466fea08f2c239750f0923ce6c1151649a6
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-26 15:23:27 -0500

    Create smallestevenmultiple.cpp

---
+++ exercises/cpp/smallestevenmultiple.cpp
@@ -0,0 +1,16 @@
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    int smallestEvenMultiple(int n) 
+    {
+        if (n % 2 == 1)
+        {
+            return 2*n;
+        }    
+        else
+        {
+            return n;
+        }
+    }
+};
