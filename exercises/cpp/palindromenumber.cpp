fb4cdcf7f0b5546ebc360822e59a986cfee9518d
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-30 11:05:23 -0500

    Create palindromenumber.cpp

---
+++ exercises/cpp/palindromenumber.cpp
@@ -0,0 +1,18 @@
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    bool isPalindrome(int x) 
+    {
+        string s = to_string(x); 
+        int n = s.length();
+        for (int i = 0; i < n/2; i++)
+        {
+            if (s[i] != s[n-i-1])
+            {
+                return false;
+            }
+        }    
+        return true;
+    }
+};
