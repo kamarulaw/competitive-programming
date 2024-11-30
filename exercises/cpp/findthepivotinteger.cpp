26ad79605515c161114de238968d96338d628464
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-30 12:10:45 -0500

    Create findthepivotinteger.cpp

---
+++ exercises/cpp/findthepivotinteger.cpp
@@ -0,0 +1,16 @@
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    int pivotInteger(int n) 
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if ( ((i)*(i+1))/2 == (n*(n+1))/2 - (i*(i-1))/2)
+            {
+                return i;
+            }
+        }    
+        return -1;
+    }
+};
