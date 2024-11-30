9c0481c1ae54b273b1b51e27f19b999189a72654
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-11-30 11:51:52 -0500

    Create uglynumber.cpp

---
+++ exercises/cpp/uglynumber.cpp
@@ -0,0 +1,44 @@
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    bool isprime(int n)
+    {
+        if (n == 2)
+        {
+            return true;
+        }
+        if (n < 2 || n % 2 == 0)
+        {
+            return false;
+        }
+        for (int i = 2; i <= (int)pow(n,.5)+1; i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool isUgly(int n) 
+    {
+        long long n_ = abs((long long)n);
+        map<long long,long long> m; m[2] = 1; m[3] = 1; m[5] = 1;
+        for (long long i = 2; i < n_; i++)
+        {
+            if (i > n_)
+            {
+                break;
+            }
+            while (n_ % i == 0 && isprime(i))
+            {
+                m[i]++;
+                n_ /= i;
+            }
+        } 
+        cout << m.size() << endl; 
+        return m.size() == 3;
+    }
+};
