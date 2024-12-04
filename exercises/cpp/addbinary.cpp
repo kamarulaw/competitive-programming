ce7068a3d502f35c95b463fbe1fde5f5fe4639cf
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-04 18:40:44 -0500

    Create addbinary.cpp

---
+++ exercises/cpp/addbinary.cpp
@@ -0,0 +1,23 @@
+/* SOLUTION 1 */
+/* psf */
+class Solution {
+public:
+    string addBinary(string a, string b) 
+    {
+        int alen = a.length();
+        int blen = b.length(); 
+        int min_ = min(alen,blen);
+        int max_ = max(alen,blen);
+        bool alonger = true;
+        if (alen != blen && blen > alen)
+        {
+            alonger = false;
+        }    
+        for (int i = min_+1; i < max_; i++)
+        {
+
+        }
+        if 
+        while ()
+    }
+};
