cc3c3809882aaa22b1f854cc8de6504314cd1b4c
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-09 16:55:04 -0500

    Create averagenumberofevenvaluesthataredivisiblebythree.cpp

---
+++ exercises/cpp/averagenumberofevenvaluesthataredivisiblebythree.cpp
@@ -0,0 +1,21 @@
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) 
+    {
+        map<int,int> m;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            m[nums[i]]++;
+        }
+        for (auto it : m)
+        {
+            if (it.second % 2 == 1)
+            {
+                return false;
+            }
+        }    
+        return true;
+    }
+};
