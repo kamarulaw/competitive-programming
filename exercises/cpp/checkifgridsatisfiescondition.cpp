0ea2c1ee44a0e54ab2fd8dc6e45421214505df08
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-14 13:38:48 -0500

    Create checkifgridsatisfiescondition.cpp

---
+++ exercises/cpp/checkifgridsatisfiescondition.cpp
@@ -0,0 +1,31 @@
+/* SOLUTION 1 */
+/* psf */
+class Solution {
+public:
+    bool satisfiesConditions(vector<vector<int>>& grid) 
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i < m-1)
+                {
+                    if (grid[i][j] != grid[i+1][j])
+                    {
+                        return false;
+                    }
+                }
+                if (j < n-1)
+                {
+                    if (grid[i][j] != grid[i][j+1])
+                    {
+                        return false;
+                    } 
+                }
+            }
+        }   
+        return true;
+    }
+};
