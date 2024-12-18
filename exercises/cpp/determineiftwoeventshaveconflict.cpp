5a2ca56d6eb43cb9131ceea67682bb11673a115a
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-18 17:33:22 -0500

    Update determineiftwoeventshaveconflict.cpp

--- exercises/cpp/determineiftwoeventshaveconflict.cpp
+++ exercises/cpp/determineiftwoeventshaveconflict.cpp
@@ -1,17 +1,17 @@
 /* SOLUTION 1 */
-/* psf */
+/* All tests passed */
 class Solution {
 public:
     bool haveConflict(vector<string>& event1, vector<string>& event2) 
     {
-        int h1start = stoi(event1[0].substr(1,2));
+        int h1start = stoi(event1[0].substr(0,2));
         int m1start = stoi(event1[0].substr(3,2));
-        int h1end__ = stoi(event1[1].substr(1,2));
+        int h1end__ = stoi(event1[1].substr(0,2));
         int m1end__ = stoi(event1[1].substr(3,2));
 
-        int h2start = stoi(event2[0].substr(1,2));
+        int h2start = stoi(event2[0].substr(0,2));
         int m2start = stoi(event2[0].substr(3,2));   
-        int h2end__ = stoi(event2[1].substr(1,2));
+        int h2end__ = stoi(event2[1].substr(0,2));
         int m2end__ = stoi(event2[1].substr(3,2));
         vector<bool> t1(60*24,false);
         vector<bool> t2(60*24,false);
