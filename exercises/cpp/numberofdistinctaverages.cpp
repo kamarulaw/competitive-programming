b4ce10df43a195e4534c53e661c46a2eff650f0d
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-21 13:14:02 -0500

    Update numberofdistinctaverages.cpp

--- exercises/cpp/numberofdistinctaverages.cpp
+++ exercises/cpp/numberofdistinctaverages.cpp
@@ -20,3 +20,46 @@ public:
         return avgs.size();
     }
 };
+
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    int distinctAverages(vector<int>& nums) 
+    {
+        int sol = 0;
+        map<string,int> m;
+        while (nums.size() > 0)
+        {
+            int min_elem = INT_MAX;
+            int max_elem = INT_MIN;
+            int min_index = INT_MAX;
+            int max_index = INT_MAX;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (nums[i] < min_elem)
+                {
+                    min_elem = nums[i];
+                    min_index = i;
+                }
+                if (nums[i] > max_elem)
+                {
+                    max_elem = nums[i];
+                    max_index = i;
+                }
+            }
+            int avg = (max_elem + min_elem) / 2;
+            string avg_ = "";
+            avg_ += to_string(avg);
+            if ((max_elem + min_elem) % 2 == 1)
+            {
+                avg_ += '.'; avg_ += '5';
+            }
+            m[avg_]++;
+            vector<int> remove = {min_index, max_index}; sort(remove.begin(),remove.end());
+            nums.erase(nums.begin() + remove[1]);
+            nums.erase(nums.begin() + remove[0]);
+        }     
+        return m.size();
+    }
+};
