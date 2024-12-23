11f77e093d6adb01d5a776523915637a8d28ef4d
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-23 13:10:15 -0500

    Create furthestpointfromorigin.cpp

---
+++ exercises/cpp/furthestpointfromorigin.cpp
@@ -0,0 +1,53 @@
+/* SOLUTION 1 */
+/* MEMORY LIMIT EXCEEDED ON CASE 1217 of 1252 */
+class Solution {
+public:
+    vector<string> strings;
+
+    void generate(int i, int k, string moves, vector<int> blanks) 
+    {
+        if (i == k)
+        {
+            strings.push_back(moves);
+            return;
+        }
+        int blank_index = blanks[i];
+        moves[blank_index] = 'L';
+        generate(i+1,k,moves,blanks);
+        moves[blank_index] = 'R';
+        generate(i+1,k,moves,blanks);
+    }
+
+    int furthestDistanceFromOrigin(string moves) 
+    {
+        int n = moves.length();
+        vector<int> blanks;
+        for (int i = 0; i < n; i++)
+        {
+            if (moves[i] == '_')
+            {
+                blanks.push_back(i);
+            }
+        }
+        int k = blanks.size();
+        int max_dfo = INT_MIN; 
+        generate(0,k,moves,blanks);
+        for (int i = 0; i < strings.size(); i++)
+        {
+            int dfo = 0;
+            for (int j = 0; j < strings[i].length(); j++)
+            {
+                if (strings[i][j] == 'L')
+                {
+                    dfo--;
+                }
+                else 
+                {
+                    dfo++;
+                }
+            }
+            max_dfo = max(max_dfo, abs(dfo));
+        }
+        return max_dfo;
+    }
+};
