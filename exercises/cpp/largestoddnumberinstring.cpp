5159e71c6fbc7075757f568adc2a26ca1d2b1527
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-23 14:20:27 -0500

    Update largestoddnumberinstring.cpp

--- exercises/cpp/largestoddnumberinstring.cpp
+++ exercises/cpp/largestoddnumberinstring.cpp
@@ -29,3 +29,38 @@ public:
         return "";
     }
 };
+
+/* SOLUTION 2 */
+/* RUNTIME ERROR ON CASE 105 of 196 */
+class Solution {
+public:
+    string largestOddNumber(string num) 
+    {
+        int n = num.length();
+        vector<int> odds;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j <= n-i; j++)
+            {
+                if (j == 1 && (num[i] - '0') % 2 == 1)
+                {
+                    odds.push_back(num[i] - '0');
+                }
+                if (num.substr(i,j).length() > 1 && stoi(num.substr(i,j)) % 2 == 1)
+                {
+                    odds.push_back(stoi(num.substr(i,j)));
+                }
+                
+            }
+        }   
+        if (odds.size() >= 1)
+        {
+            sort(odds.begin(),odds.end());
+            return to_string(odds[odds.size()-1]);
+        }
+        else
+        {
+            return "";
+        }
+    }
+};
