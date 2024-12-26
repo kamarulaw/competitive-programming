76ae55ba72082ac5e083027c34d8769a639cb514
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-26 12:49:39 -0500

    Create convertthetemperature.cpp

---
+++ exercises/cpp/convertthetemperature.cpp
@@ -0,0 +1,9 @@
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) 
+    {
+        return {celsius+273.15,celsius*1.80+32.00};    
+    }
+};
