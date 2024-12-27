a77ab4ed8a477120e584b41a4139e6a147400b14
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-27 13:43:13 -0500

    Update ispalindrome.cpp

--- exercises/cpp/ispalindrome.cpp
+++ exercises/cpp/ispalindrome.cpp
@@ -1,3 +1,5 @@
+/* SOLUTION 1 - 09/01/21*/
+/* All tests passed */
 class Solution {
 public:
   bool isPalindrome(string s) { 
@@ -10,4 +12,36 @@ public:
         return false; 
     return true; 
   }
+};
+
+/* SOLUTION 2 - 12/27/24 */
+/* All tests passed */
+class Solution {
+public:
+    bool isPalindrome(string s) 
+    {
+        string s_ = "";
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (!((0 <= s[i]-'0' && s[i]-'0' <= 9) 
+            || ((int)'a' <= s[i] && s[i] <= (int)'z') 
+            || ((int)'A' <= s[i] && s[i] <= (int)'Z')))
+            {
+                continue;
+            }
+            else
+            {
+                s_ += tolower(s[i]);
+            }
+        }
+        int n = s_.length();
+        for (int i = 0; i < n/2; i++)
+        {
+            if (s_[i] != s_[n-1-i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 };
