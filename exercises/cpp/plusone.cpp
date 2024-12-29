daefdd7a3b4e407295a482ac8b785a1b7794ad80
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-29 14:30:55 -0500

    Update plusone.cpp

--- exercises/cpp/plusone.cpp
+++ exercises/cpp/plusone.cpp
@@ -34,3 +34,30 @@ public:
         }
     }
 };
+
+/* SOLUTION 2 */
+/* All tests passed */
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) 
+    {
+        int n = digits.size();
+        int i = n-1;
+        while (i >= 0 && digits[i] == 9)
+        {
+            digits[i] = 0;
+            i--; 
+        }    
+        if (i < 0)
+        {
+            reverse(digits.begin(),digits.end());
+            digits.push_back(1);
+            reverse(digits.begin(),digits.end());
+        }
+        else 
+        {
+            digits[i] = digits[i] + 1;
+        }
+        return digits;
+    }
+};
