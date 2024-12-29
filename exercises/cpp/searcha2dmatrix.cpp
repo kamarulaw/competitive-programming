7dc89788f71627556f0b692a1895139435f2a153
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-29 13:19:47 -0500

    Update searcha2dmatrix.cpp

--- exercises/cpp/searcha2dmatrix.cpp
+++ exercises/cpp/searcha2dmatrix.cpp
@@ -37,3 +37,37 @@ public:
         return false;
     }
 };
+
+/* SOLUTION 2 */
+/* All tests passed */
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int n_ = m*n;
+        int lo = 0;
+        int hi = n_-1;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            int mid_row = mid / n;
+            int mid_col = mid % n;
+            if (matrix[mid_row][mid_col] == target)
+            {
+                return true;
+            }
+            else if (matrix[mid_row][mid_col] < target)
+            {
+                lo = mid + 1;
+            }
+            else 
+            {
+                hi = mid - 1;
+            }
+
+        }
+        return false;    
+    }
+};
