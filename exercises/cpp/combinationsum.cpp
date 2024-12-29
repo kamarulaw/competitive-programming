17da919be8d24349ebe85f6924e3ff1feb9b52a3
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-29 15:47:10 -0500

    Update combinationsum.cpp

--- exercises/cpp/combinationsum.cpp
+++ exercises/cpp/combinationsum.cpp
@@ -35,3 +35,47 @@ public:
         return combinations;
     }
 };
+
+/* SOLUTION 2 */
+/* All tests passed */
+class Solution {
+public:
+    vector<int> nums_;
+    set<vector<int>> answer;
+
+    void generate(vector<int> subset, int target) 
+    {
+        if (target == 0)
+        {
+            sort(subset.begin(),subset.end());
+            answer.emplace(subset);
+            return;
+        }
+        if (target < 0)
+        {
+            return;
+        }
+        if (target > 0)
+        {
+            for (int i = 0; i < nums_.size(); i++)
+            {
+                if (target-nums_[i] >= 0)
+                {
+                    subset.push_back(nums_[i]);
+                    generate(subset,target-nums_[i]);
+                    subset.pop_back();
+                }
+            }
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) 
+    {
+        int n = nums.size();
+        nums_ = nums;
+        generate({},target);
+        vector<vector<int>> vanswer(answer.begin(),answer.end());
+        return vanswer;
+    }
+};
+
