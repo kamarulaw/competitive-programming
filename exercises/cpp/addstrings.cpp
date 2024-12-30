a3257dbf8bc9a9b1c013e4edb96be0a89c68f828
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-12-30 14:13:13 -0500

    Update addstrings.cpp

--- exercises/cpp/addstrings.cpp
+++ exercises/cpp/addstrings.cpp
@@ -11,11 +11,21 @@ public:
 };
 
 /* SOLUTION 2 */
-/* psf */
+/* All tests passed */
 class Solution {
 public:
     string addStrings(string num1, string num2) 
     {
+        if (num1.compare("0") == 0)
+        {
+            return num2;
+        }
+        if (num2.compare("0") == 0)
+        {
+            return num1;
+        }
+        map<int,char> m;
+        m[0]='0';m[1]='1';m[2]='2';m[3]='3';m[4]='4';m[5]='5';m[6]='6';m[7]='7';m[8]='8';m[9]='9';
         reverse(num1.begin(),num1.end());
         reverse(num2.begin(),num2.end());
         while (num1.length() < num2.length())
@@ -34,8 +44,17 @@ public:
             int d2 = num2[i] - '0';
             int sum = d1 + d2 + carry;
             int d_ = sum % 10;
-            ans += 
+            ans += m[d_];
             carry = (d1 + d2 + carry) / 10;
         }
+        ans += m[carry];
+        int i = ans.length()-1;
+        while (i >= 0 && ans[i] == '0')
+        {
+            i--;
+        }
+        ans = ans.substr(0,i+1);
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
