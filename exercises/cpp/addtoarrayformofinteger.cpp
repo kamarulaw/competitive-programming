4f826b7118b9e2eb0ac9371b69431d96df324816
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-01-04 14:00:26 -0500

    Update addtoarrayformofinteger.cpp

--- exercises/cpp/addtoarrayformofinteger.cpp
+++ exercises/cpp/addtoarrayformofinteger.cpp
@@ -1,5 +1,5 @@
 /* SOLUTION 1 */
-/* psf */
+/* RUNTIME ERROR ON CASE 3 of 152 */
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) 
@@ -19,5 +19,47 @@ public:
         }
         reverse(sol.begin(),sol.end());
         return sol;
+    }
+};
+
+/* SOLUTION 2 */
+/* All tests passed */
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) 
+    {
+        vector<int> k_;
+        while (k > 0)
+        {
+            k_.push_back(k % 10);
+            k /= 10;
+        }    
+        while (k_.size() < num.size())
+        {
+            k_.push_back(0);
+        }
+        if (num.size() < k_.size())
+        {
+            reverse(num.begin(),num.end());
+            while (num.size() < k_.size())
+            {
+                num.push_back(0);
+            }
+            reverse(num.begin(),num.end());
+        }
+        reverse(num.begin(),num.end());
+        int carry = 0;
+        vector<int> result(k_.size(),0);
+        for (int i = 0; i < k_.size(); i++)
+        {
+            result[i] = ((carry + k_[i] + num[i]) % 10);
+            carry = ((carry + k_[i] + num[i]) / 10);
+        }
+        if (carry != 0)
+        {
+            result.push_back(carry);
+        }
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
