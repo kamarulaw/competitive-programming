35d61311cbe81b3a9a9fead78d8ea0a0efd7e81b
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-01-11 15:53:15 -0500

    Create defusethebomb.cpp

---
+++ exercises/cpp/defusethebomb.cpp
@@ -0,0 +1,50 @@
+/* SOLUTION 1 */
+/* psf */
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) 
+    {
+        vector<int> sol(code.size(),0);
+        if (k == 0)
+        {
+            return sol;
+        }    
+        else if (k > 0)
+        {
+            for (int i = 0; i < code.size(); i++)
+            {
+                int total = 0;
+                for (int j = i+1; j < i+k+1; j++)
+                {
+                    total += code[j%code.size()];
+                }
+                sol[i] = total;
+            }
+        }
+        else
+        {
+            for (int i = code.size(); i < 2*code.size(); i++)
+            {
+                vector<int> newcode(2*code.size(), 0);
+                for (int j = 0; j < newcode.size(); j++)
+                {
+                    if (i < code.size())
+                    {
+                        newcode[i] = code[i];
+                    }
+                    else 
+                    {
+                        newcode[i] = code[i - code.size()];
+                    }
+                }
+                int total = 0;
+                for (int j = i-1; j >= i-1-k; j--)
+                {
+                    total += newcode[j];
+                }
+                sol[i] = total;
+            }
+        }
+        return sol;
+    }
+};
