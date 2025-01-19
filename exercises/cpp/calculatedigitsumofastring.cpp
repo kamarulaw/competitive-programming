2ac8a9f9bce8139eec6bd55baf28f011f4993557
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-01-19 14:58:00 -0500

    Update calculatedigitsumofastring.cpp

--- exercises/cpp/calculatedigitsumofastring.cpp
+++ exercises/cpp/calculatedigitsumofastring.cpp
@@ -55,3 +55,57 @@ public:
         return s;    
     }
 };
+
+/* SOLUTION 2 */
+/* psf */
+class Solution {
+public:
+    string digitSum(string s, int k) 
+    {
+        while (s.length() > k)
+        {
+            int ind = 0;
+            string curr = "";
+            vector<string> groups;
+            while (ind < s.length())
+            {
+                
+                if (curr.length() < k)
+                {
+                    curr += s[ind];
+                }
+                if (curr.length() == k || ind == s.length()-1)
+                {
+                    groups.push_back(curr); curr = "";
+                }
+                ind++;
+            }
+            for (int i = 0; i < groups.size(); i++)
+            {
+                cout << groups[i] << " ";
+            }
+            cout << endl;
+            string news = "";
+            for (int i = 0; i < groups.size(); i++)
+            {
+                int sum = 0;
+                string num = groups[i];
+                for (int j = 0; j < num.length(); j++)
+                {
+                    sum += num[i] - '0';
+                }
+                if (news.length() == 0)
+                {
+                    news = to_string(sum);
+                }
+                else 
+                {
+                    news += to_string(sum);
+                }
+                
+            }
+            s = news;
+        }  
+        return s;   
+    }
+};
