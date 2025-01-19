ef14112328a81dcadad4fbd2deaa770d505bcd7c
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-01-19 15:35:47 -0500

    Update deletecharacterstomakefancystring.cpp

--- exercises/cpp/deletecharacterstomakefancystring.cpp
+++ exercises/cpp/deletecharacterstomakefancystring.cpp
@@ -49,3 +49,51 @@ public:
         return s;
     }
 };
+
+/* SOLUTION 2 */
+/* TIME LIMIT EXCEEDED ON CASE 242 of 306 */
+class Solution {
+public:
+    string editedString(string s, int i)
+    {
+        int n = s.length();
+        string ans = "";
+        for (int j = 0; j < n; j++)
+        {
+            if (j != i)
+            {
+                ans += s[j];
+            }
+        }
+        return ans;
+    }
+
+    string makeFancyString(string s) 
+    {
+        bool flag = true;
+        while (flag)
+        {
+            flag = false;
+            int n = s.length();
+            int consec = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (s[i] == s[i-1])
+                {
+                    consec++;
+                    if (consec == 3)
+                    {
+                        s = editedString(s,i);
+                        flag = true;
+                        break;
+                    }
+                }
+                if (s[i] != s[i-1])
+                {
+                    consec = 1;
+                }
+            }
+        }
+        return s;
+    }
+};
