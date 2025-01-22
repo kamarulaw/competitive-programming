0f33b617480bd72fe2c595edaab522c56dcfcd92
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-01-22 13:49:19 -0500

    Create countnicepairsinanarray.cpp

---
+++ exercises/cpp/countnicepairsinanarray.cpp
@@ -0,0 +1,43 @@
+/* SOLUTION 1 */ 
+/* TIME LIMIT EXCEEDED ON CASE 64 of 84 */
+class Solution {
+public:
+    int rev(int x)
+    {
+        vector<int> v;
+        while (x > 0)
+        {
+            v.push_back(x % 10); x /= 10;
+        }
+        int ans = 0;
+        for (int i = 0; i < v.size(); i++)
+        {
+            ans += v[i]*pow(10,v.size()-i-1);
+        }
+        return ans;
+    }
+
+    int countNicePairs(vector<int>& nums) 
+    {
+        int ans = 0;
+        map<int,int> m;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            m[nums[i]] = rev(nums[i]);
+            cout << nums[i] << " " << m[nums[i]] << endl;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                if (nums[i] + m[nums[j]] == nums[j] + m[nums[i]])
+                {
+                    ans++;
+                    ans = ans % ((int)pow(10,9)+7);
+                }
+            }
+        }
+        return ans;
+    }
+};
