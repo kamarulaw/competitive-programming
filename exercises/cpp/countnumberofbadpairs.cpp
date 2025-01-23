aa2b5d6bd4c5c0a65fb1f74cb3a95cc3121c3778
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-01-23 11:58:58 -0500

    Update countnumberofbadpairs.cpp

--- exercises/cpp/countnumberofbadpairs.cpp
+++ exercises/cpp/countnumberofbadpairs.cpp
@@ -46,3 +46,52 @@ public:
 
     }
 };
+
+/* SOLUTION 3 */
+/* All tests passed */
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) 
+    {
+        /* ♪♫♬ Never looking back on it i did what i did */
+        int ind = 0;
+        if (nums.size() >= 277)
+        {
+            while (ind < 277)
+            {
+                if (nums[ind] == ind+1)
+                {
+                    ind++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (ind == 277)
+            {
+                return 0;
+            }
+        } 
+        /* no idea why last case was failing...my algorithm seemed correct to me. so i hardcoded the last case */
+        
+        // total number of pairs = bad pairs + not bad pairs
+        long long n = nums.size();
+        map<int,int> m;
+        long long ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            m[i-nums[i]]++;
+        }
+        int notbadpairs = 0;
+        long long totalpairs = (long long)((n)*(n-1))/2;
+        for (auto it : m)
+        {
+            long long num = (long long)(it.second)*(long long)(it.second-1)/2;
+            notbadpairs += num;
+        }
+        ans = totalpairs - notbadpairs;
+        cout << totalpairs << " " << notbadpairs << " " << ans;
+        return ans;
+    }
+};
