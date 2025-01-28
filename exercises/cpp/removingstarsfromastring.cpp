30422a02a333e72e60408d8f5afb1ae19eb35646
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-01-28 14:30:02 -0500

    Update removingstarsfromastring.cpp

--- exercises/cpp/removingstarsfromastring.cpp
+++ exercises/cpp/removingstarsfromastring.cpp
@@ -1,3 +1,5 @@
+/* SOLUTION 1 */
+/* All tests passed */
 class Solution {
 public:
     string removeStars(string s) {
@@ -25,5 +27,42 @@ public:
     bool is_star(char c)
     {
         return c=='*';
+    }
+};
+
+/* SOLUTION 2 */
+/* TIME LIMMIT EXCCEDED ON CASE 44 of 65 */
+class Solution {
+public:
+    string removeStars(string s) 
+    {
+        bool containsstar = true;
+        while (containsstar)
+        {
+            int n = s.length();
+            int starind = INT_MIN;
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] == '*')
+                {
+                    starind = i; break;
+                }
+            }
+            if (starind == INT_MIN)
+            {
+                return s;
+            }
+            else 
+            {
+                int ind = starind;
+                while (ind >= 0 && s[ind] == '*')
+                {
+                    ind--;
+                }
+                s.erase(s.begin() + starind);
+                s.erase(s.begin() + ind);
+            }
+        }
+        return s;    
     }
 };
