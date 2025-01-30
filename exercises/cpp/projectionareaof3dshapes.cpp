a3f00fcfa5356bd103445ce7c60085bd1abb7d04
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-01-30 18:17:19 -0500

    Create projectionareaof3dshapes.cpp

---
+++ exercises/cpp/projectionareaof3dshapes.cpp
@@ -0,0 +1,11 @@
+/* SOLUTION 1 */
+/* psf */
+class Solution {
+public:
+    int projectionArea(vector<vector<int>>& grid) 
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        int x_area = m*n;  
+    }
+};
