7ef4352af62d74271c97c7ed206ec04d684ab89b
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-01-31 16:49:04 -0500

    Create againtwentyfive.cpp

---
+++ exercises/cpp/againtwentyfive.cpp
@@ -0,0 +1,14 @@
+/* SOLUTION 1 */
+/* All tests passed */ 
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    unsigned long long int n; cin >> n; cout << 25; return 0;
+}
