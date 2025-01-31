8038acfb226d643e760ca4a4646bb4550006b989
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-01-30 19:00:31 -0500

    Update partitionarrayintothreepartswithequalsum.cpp

--- exercises/cpp/partitionarrayintothreepartswithequalsum.cpp
+++ exercises/cpp/partitionarrayintothreepartswithequalsum.cpp
@@ -58,3 +58,70 @@ public:
         return false;
     }
 };
+
+/* SOLUTION 2 */
+/* psf */
+class Solution {
+public:
+    int arraysum(vector<int> arr)
+    {
+        int total = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            total += arr[i];
+        }
+        return total;
+    }
+
+    bool canThreePartsEqualSum(vector<int>& arr) 
+    {
+        int n = arr.size();
+        vector<int> v1;
+        vector<int> v2;
+        vector<int> v3;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                for (int k = j+1; j < n; j++)
+                {
+                    for (int ii = i; ii < j-i; ii++)
+                    {
+                        v1.push_back(arr[ii]);
+                    }
+                    for (int jj = j; jj < k-j; jj++)
+                    {
+                        v2.push_back(arr[jj]);
+                    }
+                    for (int kk = k; kk < n; kk++)
+                    {
+                        v3.push_back(arr[kk]);
+                    }
+                }
+            }
+            if (arraysum(v1) == arraysum(v2) && arraysum(v2) == arraysum(v3))
+            {
+                return true;
+            }
+            for (int p = 0; p < v1.size(); p++)
+            {
+                cout << v1[p] << " ";
+            }
+            cout << endl << endl;
+            for (int p = 0; p < v2.size(); p++)
+            {
+                cout << v2[p] << " "; 
+            }
+            cout << endl << endl;
+            for (int p = 0; p < v3.size(); p++)
+            {
+                cout << v3[p] << " "; 
+            }
+            cout << endl << endl;
+            v1.clear();
+            v2.clear();
+            v3.clear();
+        }
+        return false;
+    }
+};
