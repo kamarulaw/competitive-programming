b4572b028435db90745b7e3a95d9d9b39f5d11b4
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-02-04 12:33:19 -0500

    Create insomniacure.cpp

---
+++ exercises/cpp/insomniacure.cpp
@@ -0,0 +1,23 @@
+/* SOLUTION 1 */
+/* All tests passed */ 
+
+#include <climits>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sol = 0;
+    int k,l,m,n,d;
+    cin >> k >> l >> m >> n >> d;
+    for (int i = 1; i <= d; i++)
+    {
+        if (i%k==0||i%l==0||i%m==0||i%n==0)
+        {
+            sol++;
+        }
+    }
+    cout << sol;
+    return 0;
+}
