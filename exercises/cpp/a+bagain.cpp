228d8fe3ddd82dfe50d8e5703c8021b5ba73c432
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-02-05 11:55:52 -0500

    Create a+bagain.cpp

---
+++ exercises/cpp/a+bagain.cpp
@@ -0,0 +1,19 @@
+/* SOLUTION 1 */
+/* In queue */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        cout << x%10 + x/10 << endl;
+    }
+    return 0;
+}
