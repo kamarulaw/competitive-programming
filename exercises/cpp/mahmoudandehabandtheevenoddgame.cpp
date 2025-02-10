228a47fcff20f597a8ea5ae46d22931b3bbb8506
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-02-10 15:52:25 -0500

    Create mahmoudandehabandtheevenoddgame.cpp

---
+++ exercises/cpp/mahmoudandehabandtheevenoddgame.cpp
@@ -0,0 +1,21 @@
+/* SOLUTION 1 */
+/* All tests passed */
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n%2==0)
+    {
+        cout << "Mahmoud";
+    }
+    else
+    {
+        cout << "Ehab";
+    }
+}
