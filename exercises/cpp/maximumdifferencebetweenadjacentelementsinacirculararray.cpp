72d73346ad1e6d4d7f8836301c04edf494220d6f
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-02-28 12:49:09 -0500

    Create maximumdifferencebetweenadjacentelementsinacirculararray.cpp

---
+++ exercises/cpp/maximumdifferencebetweenadjacentelementsinacirculararray.cpp
@@ -0,0 +1,16 @@
+/* SOLUTION 1 */
+/* All tests passed */
+
+class Solution {
+public:
+    int maxAdjacentDistance(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int sol = abs(nums[n-1]-nums[0]);
+        for (int i = 1; i < n; i++)
+        {
+            sol = max(abs(nums[i]-nums[i-1]),sol);
+        }    
+        return sol;
+    }
+};
