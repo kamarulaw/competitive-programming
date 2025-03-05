4bade158916335dca5d3677c84c4bf2e42bdda48
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-03-05 16:23:09 -0500

    Update countingbits.cpp

--- exercises/cpp/countingbits.cpp
+++ exercises/cpp/countingbits.cpp
@@ -19,3 +19,27 @@ public:
         return result;
     }
 };
+
+/* SOLUTION 2 */
+/* All tests passed */
+class Solution {
+public:
+    vector<int> countBits(int n) 
+    {
+        vector<int> sol(n+1,0);
+        for (int i = 0; i <= n; i++)
+        {
+            int sbc = 0;
+            string rep = bitset<32>(i).to_string();
+            for (int j = 0; j < rep.length(); j++)
+            {
+                if (rep[j]=='1')
+                {
+                    sbc++;
+                }
+            }
+            sol[i] = sbc;
+        }    
+        return sol;
+    }
+};
