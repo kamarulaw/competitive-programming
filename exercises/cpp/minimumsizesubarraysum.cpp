e176cea81e6570d70704b62812be6167089020ce
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-03-07 15:53:50 -0500

    Update minimumsizesubarraysum.cpp

--- exercises/cpp/minimumsizesubarraysum.cpp
+++ exercises/cpp/minimumsizesubarraysum.cpp
@@ -32,3 +32,46 @@ public:
         return minsubarraysize;    
     }
 };
+
+/* SOLUTION 2 */
+/* MEMORY LIMIT EXCEEDED ON CASE 18 of 21 */
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) 
+    {
+        int sol = INT_MAX;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j <= n-i; j++)
+            {
+                int csum;
+                vector<int> arr(nums.begin()+i,nums.begin()+i+j);
+                for (int k = 0; k < j; k++)
+                {
+                    if (k==0)
+                    {
+                        csum = arr[k];
+                    }
+                    else
+                    {
+                        csum += arr[k];
+                    }
+                }
+                if (csum>=target)
+                {
+                    sol = min(sol,(int)arr.size());
+                }
+            }
+        }    
+        if (sol==INT_MAX)
+        {
+            return 0;
+        }
+        else
+        {
+            return sol;
+        }
+    }
+};
