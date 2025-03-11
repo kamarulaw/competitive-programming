bcd77dcdae91b5300bfb7bc7c692a5aad8c0fe98
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2025-03-11 19:01:11 -0400

    Update convertdatetobinary.cpp

--- exercises/cpp/convertdatetobinary.cpp
+++ exercises/cpp/convertdatetobinary.cpp
@@ -33,3 +33,56 @@ public:
         int d_ = stoi(d);
     }
 };
+
+/* SOLUTION 2 */
+/* All tests passed */
+
+class Solution {
+public:
+    string convertDateToBinary(string date) 
+    {
+        int yy = stoi(date.substr(0,4));
+        int mm = stoi(date.substr(5,2));
+        int dd = stoi(date.substr(8,2));
+        string yr = bitset<32>(yy).to_string();
+        string mr = bitset<32>(mm).to_string();
+        string dr = bitset<32>(dd).to_string();
+        int y_rb = INT_MAX;
+        int m_rb = INT_MAX;
+        int d_rb = INT_MAX;
+        for (int i = 0; i < yr.length(); i++)
+        {
+            if (yr[i]=='1')
+            {
+                y_rb = i;
+                break;
+            }
+        }
+        for (int i = 0; i < mr.length(); i++)
+        {
+            if (mr[i]=='1')
+            {
+                m_rb = i;
+                break;
+            }
+        }
+        for (int i = 0; i < dr.length(); i++)
+        {
+            if (dr[i]=='1')
+            {
+                d_rb = i;
+                break;
+            }
+        }
+        yr = yr.substr(y_rb,yr.length()-y_rb);
+        mr = mr.substr(m_rb,mr.length()-m_rb);
+        dr = dr.substr(d_rb,dr.length()-d_rb);
+        string sol = "";
+        sol+=yr;
+        sol+='-';
+        sol+=mr;
+        sol+='-';
+        sol+=dr;
+        return sol;
+    }
+};
