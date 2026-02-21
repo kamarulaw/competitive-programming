ed59412e41e55084f6c111989ce41887a0ec9e77
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2026-02-21 12:43:07 -0500

    Create computealternatingsum.cpp

---
+++ exercises/cpp/computealternatingsum.cpp
@@ -0,0 +1,21 @@
+/* SOLUTION 1 */
+/* All tests passed */
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) 
+    {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i%2==0)
+            {
+                ans += nums[i];
+            }
+            else
+            {
+                ans -= nums[i];
+            }
+        }    
+        return ans;
+    }
+};
