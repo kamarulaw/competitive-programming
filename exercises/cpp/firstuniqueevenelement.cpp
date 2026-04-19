2ea5f4389d05dd5822d512457d5f9e231c8899a4
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2026-04-19 13:08:11 -0400

    Add function to find first unique even element
    Implemented a function to find the first unique even element in a vector of integers.

--- exercises/cpp/firstuniqueevenelement.cpp
+++ exercises/cpp/firstuniqueevenelement.cpp
@@ -27,3 +27,41 @@ public:
         for ()    
     }
 };
+
+/* Solution 2 */
+/* All tests passed */
+
+// 🎶 I wasnt made for no casket or no prison cell...
+    
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) 
+    {
+        map<int,int> m;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++)
+        {
+            m[nums[i]]++;
+        }
+
+        map<int,int> refined_map;
+
+        for (auto it : m)
+        {
+            if (it.first%2==0 && it.second==1)
+            {
+                refined_map[it.first] = 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (refined_map[nums[i]]==1)
+            {
+                return nums[i];
+            }
+        }    
+        return -1;
+    }
+};
