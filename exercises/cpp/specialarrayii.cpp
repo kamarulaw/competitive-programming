52e98e5376a78abfb6ded1a9dedc9ecddba55799
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2026-04-21 14:03:26 -0400

    Update specialarrayii.cpp

--- exercises/cpp/specialarrayii.cpp
+++ exercises/cpp/specialarrayii.cpp
@@ -90,3 +90,44 @@ public:
         return solution_vector;
     }
 };
+
+/* SOLUTION 4 */
+/* TIME LIMIT EXCEEDED ON CASE 535 of 536 */
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) 
+    {
+        vector<bool> solution_vector;
+        int num_queries = queries.size();
+        int n = nums.size();
+        map<int,int> upperbounds;
+        for (int i = 0; i < n-1; i++)
+        {
+            if (nums[i]%2==nums[i+1]%2)
+            {
+                upperbounds[i]++;
+            }
+        }
+        for (int i = 0; i < num_queries; i++)
+        {
+            bool index_solution = true;
+            int first_index = queries[i][0];
+            int last_index = queries[i][1];
+            if (first_index == last_index)
+            {
+                solution_vector.push_back(true);
+                continue;
+            }
+            for (int ii = first_index; ii <= last_index; ii++)
+            {
+                if (upperbounds[ii]>=1 && ii != last_index)
+                {
+                    index_solution = false;
+                    break;
+                }
+            }
+            solution_vector.push_back(index_solution);
+        }    
+        return solution_vector;
+    }
+};
