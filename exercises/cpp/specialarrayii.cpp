a5353c06de4ba2230f4fa9ba0694ec138b9f8766
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2026-04-21 13:22:42 -0400

    Implement second solution for special array check
    Added a second solution for checking special arrays, though it currently exceeds time limits on specific cases.

--- exercises/cpp/specialarrayii.cpp
+++ exercises/cpp/specialarrayii.cpp
@@ -28,3 +28,29 @@ public:
         return solution_vector;
     }
 };
+
+/* SOLUTION 2 */
+/* TIME LIMIT EXCEEDED ON CASE 523 of 536 */
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) 
+    {
+        vector<bool> solution_vector;
+        int num_queries = queries.size();
+        for (int i = 0; i < num_queries; i++)
+        {
+            bool index_solution = true;
+            int first_index = queries[i][0];
+            int last_index = queries[i][1];
+            for (int ii = first_index; ii < last_index; ii++)
+            {
+                if (nums[ii]%2==nums[ii+1]%2)
+                {
+                    index_solution = false;
+                }
+            }
+            solution_vector.push_back(index_solution);
+        }    
+        return solution_vector;
+    }
+};
