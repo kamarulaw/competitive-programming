9d26f38157a9921f90460be5d13d850740134aec
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2026-04-21 13:55:11 -0400

    Implement solution class for special array queries
    Added a new solution class for checking special arrays with queries, but noted a wrong answer on case 459.

--- exercises/cpp/specialarrayii.cpp
+++ exercises/cpp/specialarrayii.cpp
@@ -54,3 +54,39 @@ public:
         return solution_vector;
     }
 };
+
+/* SOLUTION 3 */
+/* WRONG ANSWER ON CASE 459 of 536 */
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) 
+    {
+        vector<bool> solution_vector;
+        int num_queries = queries.size();
+        int n = nums.size();
+        map<int,int> upperbounds;
+        for (int i = 0; i < n-1; i++)
+        {
+            if (nums[i]%2==nums[i+1]%2)
+            {
+                upperbounds[i]++;
+            }
+        }
+        for (int i = 0; i < num_queries; i++)
+        {
+            bool index_solution = true;
+            int first_index = queries[i][0];
+            int last_index = queries[i][1];
+            for (int ii = first_index; ii <= last_index; ii++)
+            {
+                if (upperbounds[ii]>=1)
+                {
+                    index_solution = false;
+                    break;
+                }
+            }
+            solution_vector.push_back(index_solution);
+        }    
+        return solution_vector;
+    }
+};
