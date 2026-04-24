662d4eceec8ad9668b5cff568d649c62b33743d6
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2026-04-24 16:07:20 -0400

    Update buttonwithlongestpresstime.cpp

--- exercises/cpp/buttonwithlongestpresstime.cpp
+++ exercises/cpp/buttonwithlongestpresstime.cpp
@@ -94,3 +94,37 @@ public:
         return result1;
     }
 };
+
+/* SOLUTION 4 */
+/* psf */
+class Solution {
+public:
+    int buttonWithLongestTime(vector<vector<int>>& events) 
+    {
+        int n = events.size(); 
+        map<int,int> presstime;
+        int elapsedtime = events[0][1];
+        presstime[events[0][0]] = elapsedtime;
+        for (int i = 1; i < n; i++)
+        {
+            elapsedtime = (events[i][1]-events[i-1][1]);
+            presstime[events[i][0]] += elapsedtime;
+        }
+        int result1 = events[0][0];
+        int result2 = presstime[result1];
+        for (auto it : presstime)
+        {
+            cout << it.first << " " << it.second << endl;
+            if (presstime[it.first] > result2)
+            {
+                result1 = it.first;
+                result2 = presstime[result1];
+            }
+            else if (presstime[it.first] == result2)
+            {
+                result1 = min(it.first, result1);
+            }
+        }
+        return result1;
+    }
+};
