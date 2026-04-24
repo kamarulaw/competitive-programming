2adec9230d2bb8e852353b076f4f8f06526fe80e
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2026-04-24 15:14:49 -0400

    Implement buttonWithLongestTime function

---
+++ exercises/cpp/buttonwithlongestpresstime.cpp
@@ -0,0 +1,37 @@
+/* SOLUTION 1 */
+/* psf */
+
+class Solution {
+public:
+    int buttonWithLongestTime(vector<vector<int>>& events) 
+    {
+        int elapsedtime = 0;
+        int n = events.size(); 
+        map<int,int> presstime;
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+            {
+                elapsedtime = events[i][1];
+            }
+            else
+            {
+                elapsedtime = (events[i][1]-elapsedtime);
+            }
+
+            presstime[events[i][0]] += elapsedtime;
+        }
+        int result1 = events[0][0];
+        int result2 = INT_MIN;
+        for (auto it : presstime)
+        {
+            cout << it.first << " " << it.second << endl;
+            if (presstime[result1] > result2)
+            {
+                result1 = it.first;
+                result2 = presstime[result1];
+            }
+        }
+        return result1;
+    }
+};
