44e507add60e4e7ac88908d2eef920179e6dea6f
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2026-04-24 14:18:57 -0400

    Update calculateamountpaidintaxes.cpp

--- exercises/cpp/calculateamountpaidintaxes.cpp
+++ exercises/cpp/calculateamountpaidintaxes.cpp
@@ -26,3 +26,35 @@ public:
 
     }
 };
+
+/* SOLUTION 2 */
+/* psf */
+
+class Solution {
+public:
+    double calculateTax(vector<vector<int>>& brackets, int income) 
+    {
+        int n = brackets.size();
+        double result = 0.0;
+        for (int i = 0; i < n; i++)
+        {
+            int amount_taxed = min(brackets[i][0],income-brackets[i][0]);
+            cout << amount_taxed << " ";
+            income = min(brackets[i][0],income-brackets[i][0]);
+            if (i == 0)
+            {
+                result += amount_taxed*brackets[i][1];
+            }
+            else 
+            {
+                result += amount_taxed*(brackets[i][1]-brackets[i-1][1]);
+            }
+
+            if (income == 0)
+            {
+                break;
+            }
+        }  
+        return result;
+    }
+};
