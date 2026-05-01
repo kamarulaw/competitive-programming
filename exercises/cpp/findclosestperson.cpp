6a21d9505d617799acf4ba4eca3f4b11bafda400
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2026-05-01 14:45:35 -0400

    Add findClosest function in findclosestperson.cpp
    Implement findClosest function to determine the closest person.

---
+++ exercises/cpp/findclosestperson.cpp
@@ -0,0 +1,21 @@
+/* SOLUTION 1 */
+/* All tests passed */
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) 
+    {
+        if (abs(x-z) < abs(y-z))
+        {
+            return 1;
+        }    
+        else if (abs(y-z) < abs(x-z))
+        {
+            return 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
