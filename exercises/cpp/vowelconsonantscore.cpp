95eb53880f7cc4140cce0f7ce18bf143c3a1a57b
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2026-05-06 12:18:47 -0400

    *

---
+++ exercises/cpp/vowelconsonantscore.cpp
@@ -0,0 +1,30 @@
+/* SOLUTION 1 */
+/* psf */
+/* leetcode (not codeforces) is down?
+class Solution {
+public:
+    int vowelConsonantScore(string s) 
+    {
+        int v = 0;
+        int c = 0;
+        int score = 0;
+        map<char,int> m;
+        m['a'] = 1;
+        m['e'] = 1;
+        m['i'] = 1;
+        m['o'] = 1;
+        m['u'] = 1;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (m[s[i]]==1)
+            {
+                v++;
+            }
+            else
+            {
+                c++;
+            }
+        }
+        return v/c;
+    }
+};
